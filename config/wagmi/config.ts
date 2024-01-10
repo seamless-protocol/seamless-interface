d6eafd9e5d5670a27a445ae2c833b48c189c9225
kitanovicd <kitanovic.danilo@outlook.com>
2024-01-10 13:18:24 +0100

    feat: remove hardocded rpc url

--- config/wagmi/config.ts
+++ config/wagmi/config.ts
@@ -6,8 +6,6 @@ export const config = createConfig({
   chains: [base],
   connectors: [injected(), metaMask(), safe()],
   transports: {
-    [base.id]: http(
-      "https://rpc.tenderly.co/fork/546513f3-f768-4d02-9ce0-83a000e9b8c3"
-    ),
+    [base.id]: http(),
   },
 });
