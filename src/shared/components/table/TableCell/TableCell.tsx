74e544ffd088655e7f3c0525cffda62fdca96610
Srdjan Rakic <bans.cowboy@gmail.com>
2024-01-26 18:00:35 +0100

    corrected tablecell and tablerow interfaces

--- src/shared/components/table/TableCell/TableCell.tsx
+++ src/shared/components/table/TableCell/TableCell.tsx
@@ -1,8 +1,14 @@
 export const TableCell: React.FC<{
   children?: React.ReactNode;
   className?: string;
-}> = ({ children, className }) => {
+  rest?: React.DetailedHTMLProps<
+    React.TdHTMLAttributes<HTMLTableCellElement>,
+    HTMLTableCellElement
+  >;
+}> = ({ children, className, rest }) => {
   return (
-    <td className={`text-center ${className ? className : ""}`}>{children}</td>
+    <td className={`text-center ${className ? className : ""}`} {...rest}>
+      {children}
+    </td>
   );
 };
--- src/shared/components/table/TableRow/TableRow.tsx
+++ src/shared/components/table/TableRow/TableRow.tsx
@@ -2,12 +2,17 @@ export const TableRow: React.FC<{
   children?: React.ReactNode;
   className?: string;
   hideBorder?: boolean;
-}> = ({ children, className, hideBorder }) => {
+  rest?: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLTableRowElement>,
+    HTMLTableRowElement
+  >;
+}> = ({ children, className, hideBorder, rest }) => {
   return (
     <tr
       className={`px-6 items-center h-[76px] cursor-pointer hover:bg-action-hover 
       ${hideBorder ? "" : "border-solid border-b border-b-divider"} 
       ${className ? className : ""}`}
+      {...rest}
     >
       {children}
     </tr>
