12a70b1e5ab2149a22dcc3a4d039228240efe982
Srdjan Rakic <bans.cowboy@gmail.com>
2024-01-30 22:23:23 +0100

    underlying assets added to interface

--- src/app/state/loop-strategy/types/ViewStrategyPageHeader.ts
+++ src/app/state/loop-strategy/types/ViewStrategyPageHeader.ts
@@ -1,6 +1,13 @@
+import { Address } from "viem";
 import { ValueSymbolPair } from "../../../../shared";
 
 export interface ViewStrategyPageHeader {
   targetMultiple: string;
   oraclePrice: ValueSymbolPair;
+  underlyingAsset: {
+    name: string;
+    symbol: string;
+    address: Address;
+    logo: string;
+  };
 }
