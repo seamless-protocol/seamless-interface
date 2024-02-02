f30684853ae281947ba2f6da67a885a17c1d59c3
WingsDevelopment <bans.cowboy@gmail.com>
2024-02-02 10:37:18 +0100

    feat: Implementing deposit modal  (#43)

---
+++ src/shared/components/button/Button.tsx
@@ -0,0 +1,122 @@
+import React, { ButtonHTMLAttributes } from "react";
+
+import { Color } from "../../types/Color";
+
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  /**
+   * Specifies the color variant of the button.
+   * @example
+   * ```jsx
+   * <Button variant="primary">Primary Button</Button>
+   * ```
+   */
+  color?: Color;
+
+  /**
+   * Specifies the size of the button.
+   * @example
+   * ```jsx
+   * <Button size="big">Big Button</Button>
+   * ```
+   */
+  size?: "small" | "normal" | "big";
+
+  /**
+   * If true, the button will take up the full width of its container.
+   * @example
+   * ```jsx
+   * <Button fullWidth={true}>Full Width Button</Button>
+   * ```
+   */
+  fullWidth?: boolean;
+
+  /**
+   * Renders spinners and disables button when set to true.
+   * @example
+   * ```jsx
+   * <Button loading>Big Button</Button>
+   * ```
+   */
+  loading?: boolean;
+
+  /**
+   * Specifies the style variant of the button: 'text', 'outlined', or 'contained'.
+   * @example
+   * ```jsx
+   * <Button variant="text">Text Button</Button>
+   * ```
+   */
+  variant?: "text" | "outlined" | "contained";
+}
+
+/**
+ * Button Component
+ *
+ * A versatile button component that can be used across the application. This component
+ * provides flexibility with different sizes, color variants, and text styles. It also
+ * provides an optional loading state which displays a spinner and disables the button.
+ *
+ * @example
+ * ```jsx
+ * <Button size="big" variant="primary" loading>
+ *   Loading Button
+ * </Button>
+ * ```
+ */
+
+// TODO: IMPLEMENT COMPONENT PROPERLY!!!
+export const Button: React.FC<ButtonProps> = ({
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  color = "primary",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  size = "normal",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  variant = "contained",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  fullWidth = false,
+  type = "button",
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  className = "",
+  loading,
+  children,
+  ...props
+}) => {
+  const classes = "";
+  // TODO: IMPLEMENT COMPONENT PROPERLY!!!
+  //     style.root,
+  //     style[size],
+  //     {
+  //       [getBgVariant(props.disabled || loading ? "disabled" : color)]:
+  //         variant === "contained",
+  //     },
+  //     {
+  //       [getBorderVariant(props.disabled || loading ? "disabled" : color)]:
+  //         variant === "outlined",
+  //     },
+  //     {
+  //       ["w-full"]: fullWidth,
+  //       ["cursor-default"]: loading,
+  //       ["text-white"]:
+  //         color !== "warning" && color !== "secondary" && variant === "contained",
+  //       ["text-black"]:
+  //         ((color === "warning" || color === "secondary") &&
+  //           variant === "contained") ||
+  //         props.disabled,
+  //     },
+  //     className
+  //   );
+
+  return (
+    <button
+      className={`${props.disabled || loading ? "bg-action-disabled" : "hover:bg-primary-dark"} 
+        bg-primary-main rounded-[4px] py-[10px] px-6 
+        text-text-primary text-description ${classes}`}
+      type={type}
+      disabled={props.disabled || loading}
+      {...props}
+    >
+      {children}
+      {loading && <div className="loading loading-spinner loading-md ml-1.5" />}
+    </button>
+  );
+};
---
+++ src/shared/components/form/inputs/InputField.tsx
@@ -0,0 +1,51 @@
+import { NumericFormat } from "react-number-format";
+import React from "react";
+import { InputFieldProps, CustomInput } from "./CustomInput";
+
+export const InputField = React.forwardRef<HTMLInputElement, InputFieldProps>(
+  ({ value, onChange, ...props }, ref) => {
+    const MAX_VAL =
+      props.max !== undefined ? props.max : Number.MAX_SAFE_INTEGER;
+    const MIN_VAL =
+      props.min !== undefined ? props.min : Number.MIN_SAFE_INTEGER;
+
+    // todo: resolve this properly
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const withValueCap = (inputObj: { floatValue: any }) => {
+      const { floatValue } = inputObj;
+      return (
+        floatValue === undefined ||
+        (floatValue <= MAX_VAL && floatValue >= MIN_VAL)
+      );
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const cleanedValue = event.target.value.replace(/,/g, "");
+      if (onChange) {
+        onChange({
+          ...event,
+          target: {
+            ...event.target,
+            value: cleanedValue,
+            name: event.target.name,
+          },
+        });
+      }
+    };
+
+    return (
+      <NumericFormat
+        type="text"
+        value={value}
+        allowLeadingZeros
+        customInput={CustomInput}
+        onChange={handleChange}
+        isAllowed={withValueCap}
+        {...ref}
+        {...props}
+      />
+    );
+  }
+);
+
+InputField.displayName = "InputField";
