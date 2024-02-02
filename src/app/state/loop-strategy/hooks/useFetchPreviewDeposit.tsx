4ed02e401909a4e4a4b8f314e3ba2af1ea1e6522
kitanovicd <kitanovic.danilo@outlook.com>
2024-02-02 16:42:39 +0100

    feat: remove console logs

--- src/app/state/loop-strategy/hooks/useFetchPreviewDeposit.tsx
+++ src/app/state/loop-strategy/hooks/useFetchPreviewDeposit.tsx
@@ -1,21 +1,14 @@
 import { Address, parseUnits } from "viem";
-import { loopStrategyAbi } from "../../../generated/generated";
-import { useReadContract } from "wagmi";
+import { useReadLoopStrategyPreviewDeposit } from "../../../generated/generated";
 
 export const useFetchPreviewDeposit = (
   strategyAddress: Address,
   amount: string
 ) => {
-  console.log("strategyAddress", strategyAddress);
-  console.log("amount", amount);
-  const { data: shares } = useReadContract({
+  const { data: shares } = useReadLoopStrategyPreviewDeposit({
     address: strategyAddress,
-    abi: loopStrategyAbi,
-    functionName: "previewDeposit",
     args: [parseUnits(amount, 18)],
   });
-
-  console.log(shares);
 
   return {
     shares: ((shares || 0n) * 60n) / 100n,
