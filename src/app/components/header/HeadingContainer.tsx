39363195c17072f2f8b41ccb9d5f7390d0baeb91
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-07 19:51:58 +0100

    fixed missing back button

--- src/app/components/header/HeadingContainer.tsx
+++ src/app/components/header/HeadingContainer.tsx
@@ -1,10 +1,18 @@
-import { PropsWithChildren } from "react";
+import { GoBackButton } from "../temporary-components/GoBackButton";
 
-export const HeadingContainer: React.FC<PropsWithChildren> = ({ children }) => {
+export const HeadingContainer: React.FC<{
+  children: React.ReactNode;
+  displayBackButton?: boolean;
+}> = ({ children, displayBackButton }) => {
   return (
     <div className="bg-background-header">
       <div className="flex flex-col xxl:items-center pt-12 pb-20">
         <div className="mx-2 lg:mx-10 xl:mx-24 xxl:w-[1440px] xxl:items-start">
+          {displayBackButton && (
+            <div className="mb-2">
+              <GoBackButton />
+            </div>
+          )}
           {children}
         </div>
       </div>
---
+++ src/app/components/temporary-components/GoBackButton.tsx
@@ -0,0 +1,24 @@
+import { FlexRow, Icon, Typography } from "../../../shared";
+import { useNavigate } from "react-router-dom";
+import leftArrow from "/common/left-arrow.svg";
+
+export const GoBackButton = () => {
+  const navigate = useNavigate();
+  return (
+    <div>
+      <button
+        className="box-border border-solid border-divider border-thin
+    rounded px-3 py-1.5  transition-all duration-250 ease-in-out 
+  bg-primary-main hover:bg-background-header"
+        onClick={() => navigate(-1)}
+      >
+        <FlexRow className="gap-2">
+          <Icon src={leftArrow} alt="left-arrow" width={20} height={12} />
+          <Typography type="secondary14" className="text-white">
+            Go Back
+          </Typography>
+        </FlexRow>
+      </button>
+    </div>
+  );
+};
