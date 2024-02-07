1e1452b17238f3dbac121d4716e5197314cc7f69
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-07 20:40:48 +0100

    authguard improved

--- src/shared/components/auth/AuthGuard.tsx
+++ src/shared/components/auth/AuthGuard.tsx
@@ -3,56 +3,64 @@ import { useAccount } from "wagmi";
 import { WalletOptions } from "../wallet/WalletOptions";
 import { FlexCol } from "../containers/FlexCol";
 import { Typography } from "../text/Typography/Typography";
-import { TypographyType } from "../text/Typography/mappers";
 
 /**
- * `AuthGuard` Component
+ * `AuthGuard` Component Documentation
  *
- * The `AuthGuard` component is designed to conditionally render content based on the user's wallet connection status. It displays a customizable message prompting the user to connect their wallet if they are not connected. Once the user connects their wallet, it renders the child components passed to it.
+ * The `AuthGuard` component is a React component designed to conditionally render its children
+ * based on the wallet connection status of a user. When the user's wallet is not connected,
+ * it displays a customizable message along with wallet connection options. Once the wallet is connected,
+ * it renders the children components, making it useful for protecting content that requires a wallet connection.
  *
- * ## Key Features:
- * - **Wallet Connection Check**: Automatically checks if the user's wallet is connected using the `useAccount` hook from `wagmi`.
- * - **Customizable Message**: Displays a message to users who have not connected their wallet, with the ability to customize this message.
- * - **Flexible Typography**: Allows the customization of the typography style of the message using the `Typography` component and `TypographyType`.
- * - **Extensibility with Custom Classes**: Accepts custom CSS classes to style the container that holds the message and wallet options.
+ * ### Key Features:
+ * - **Wallet Connection Check**: Utilizes the `useAccount` hook from `wagmi` to check the wallet's connection status.
+ * - **Customizable Message**: Provides an optional prop to customize the message shown to users without a connected wallet.
+ * - **Flexible Typography**: Uses the `Typography` component to allow customization of the message's typography, supporting different `TypographyType` values.
+ * - **Styling Flexibility**: Accepts custom CSS classes through the `className` prop to style the component's container, enhancing its adaptability to different UI designs.
  *
- * ## Props:
- * - `children`: The content to be rendered if the wallet is connected.
- * - `message`: Optional message to display when the wallet is not connected. Defaults to a generic connect wallet message.
- * - `className`: Optional string for additional custom CSS classes to style the outer container.
- * - `typography`: A `TypographyType` value to customize the typography style of the message text.
+ * ### Props:
+ * - `children` (React.ReactNode): Content or components to be rendered when the user's wallet is connected. This is the guarded content.
+ * - `message` (React.ReactNode, optional): A customizable message displayed when the wallet is not connected. Defaults to a generic message prompting the user to connect their wallet.
+ * - `className` (string, optional): A class name for additional styling of the component's outer container. Helps integrate the component into the application's styling theme.
+ * - `typography` (`TypographyType`, optional): Specifies the typography style for the message. This prop is not directly implemented in the current version but mentioned for future flexibility with the `Typography` component.
  *
- * ## Usage:
+ * ### Usage Example:
  *
  * ```jsx
- * <AuthGuard message="Custom message here" typography="h1">
- *   <YourComponent />
+ * <AuthGuard message={<Typography type="description">Please connect your wallet to access this feature.</Typography>}>
+ *   <ProtectedContent />
  * </AuthGuard>
  * ```
  *
- * In this example, the `AuthGuard` component checks if the user's wallet is connected. If not, it displays "Custom message here" with `h1` typography styling. If the wallet is connected, it renders `<YourComponent />`.
+ * In this usage example, the `AuthGuard` checks if the user's wallet is connected. If the wallet is not connected,
+ * it displays a custom message with an option to connect the wallet. Once the wallet is connected, the `ProtectedContent` component is rendered.
  *
- * @param props Props for the `AuthGuard` component.
- * @returns The `AuthGuard` component, rendering either a connect wallet message with options or the child components.
+ * @param {Object} props - The props for the `AuthGuard` component.
+ * @param {React.ReactNode} props.children - The content to be displayed upon successful wallet connection.
+ * @param {React.ReactNode} [props.message] - Optional custom message component to display when the wallet is not connected.
+ * @param {string} [props.className] - Optional custom CSS class for styling the component.
+ * @returns {React.ReactElement} The rendered `AuthGuard` component.
  */
 
 export const AuthGuard: React.FC<{
   children: React.ReactNode;
-  message?: string;
+  message?: React.ReactNode;
   className?: string;
-  typography?: TypographyType;
 }> = ({
   children,
-  message = "Please connect a wallet to view your personal information here.",
+  message = (
+    <Typography type="description">
+      Please connect a wallet to view your personal information here.
+    </Typography>
+  ),
   className = "",
-  typography = "description",
 }) => {
   const { isConnected } = useAccount();
 
   if (!isConnected)
     return (
       <FlexCol className={`gap-1 ${className}`}>
-        <Typography type={typography}>{message}</Typography>
+        {message}
         <div className="bg-background-header">
           <WalletOptions />
         </div>
