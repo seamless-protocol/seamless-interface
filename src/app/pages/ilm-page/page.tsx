dad853da2d0e8529823a7c54e6351fed746f2099
Rakic <bans.cowboy@gmail.com>
2024-02-12 17:26:11 +0100

    image prod test

--- src/app/pages/ilm-page/page.tsx
+++ src/app/pages/ilm-page/page.tsx
@@ -3,12 +3,16 @@ import { HeadingContainer } from "../../components/header/HeadingContainer";
 import { Heading } from "./components/Heading";
 import { PageContainer } from "../../../shared";
 
+import imageTest from "../../../assets/wsteth-diagram.svg";
+
 export const IlmPage = () => {
   return (
     <PageContainer>
       <HeadingContainer>
         <Heading />
       </HeadingContainer>
+      <img src={imageTest} />
+      asd
       <StrategiesTable />
     </PageContainer>
   );
