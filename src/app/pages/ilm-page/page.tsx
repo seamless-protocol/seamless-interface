83d9a622e8fe63a8e4722c7e4928fa7fd15afd86
Rakic <bans.cowboy@gmail.com>
2024-02-12 18:10:59 +0100

    testing#3

--- src/app/pages/ilm-page/page.tsx
+++ src/app/pages/ilm-page/page.tsx
@@ -3,12 +3,21 @@ import { HeadingContainer } from "../../components/header/HeadingContainer";
 import { Heading } from "./components/Heading";
 import { PageContainer } from "../../../shared";
 
+//alias
 import imageTest from "@assets/wsteth-diagram.svg";
 import cbeth from "@assets/cbeth.svg";
+
+//2
+import imageTest2 from "/public/wsteth-diagram.svg";
+import cbeth2 from "/public/cbeth.svg";
 
 //3
 import imageTest3 from "/wsteth-diagram.svg";
 import cbeth3 from "/cbeth.svg";
+
+//4
+import imageTest4 from "/wsteth-diagram.svg?url";
+import cbeth4 from "/cbeth.svg?url";
 
 export const IlmPage = () => {
   return (
@@ -16,12 +25,18 @@ export const IlmPage = () => {
       <HeadingContainer>
         <Heading />
       </HeadingContainer>
+      alias
       <img src={cbeth} />
       <img src={imageTest} />
-      asd2
+      public
+      <img src={cbeth2} />
+      <img src={imageTest2} />
+      publicr
       <img src={cbeth3} />
       <img src={imageTest3} />
-      asd3
+      public.url
+      <img src={cbeth4} />
+      <img src={imageTest4} />
       <StrategiesTable />
     </PageContainer>
   );
