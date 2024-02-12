c824cd522570aa767bde6a2c551eaed68d1700da
kitanovicd <kitanovic.danilo@outlook.com>
2024-02-12 12:15:47 +0100

    feat: update header on ilm page

--- src/app/pages/ilm-page/components/Heading.tsx
+++ src/app/pages/ilm-page/components/Heading.tsx
@@ -4,7 +4,7 @@ import { useFetchIlmHeaderInfo } from "../../../state/ILM/hooks/useFetchIlmPageH
 export const Heading = () => {
   const { isFetched, data } = useFetchIlmHeaderInfo();
   return (
-    <FlexCol className="gap-4 text-text-primary">
+    <FlexCol className="gap-5 text-text-primary">
       <FlexCol>
         <Typography type="h1">Integrated Liquidity Market</Typography>
         <Typography type="description" color="light">
@@ -18,7 +18,29 @@ export const Heading = () => {
           </Typography>
           <DisplayMoney
             typography="main21"
-            {...data.totalMarketSize}
+            {...data.totalMarketSizeUsd}
+            symbolColor="light"
+            isFetched={isFetched}
+          />
+        </FlexCol>
+        <FlexCol className="min-h-14">
+          <Typography type="description" color="light">
+            Total available
+          </Typography>
+          <DisplayMoney
+            typography="main21"
+            {...data.totalAvailableUsd}
+            symbolColor="light"
+            isFetched={isFetched}
+          />
+        </FlexCol>
+        <FlexCol className="min-h-14">
+          <Typography type="description" color="light">
+            Total borrows
+          </Typography>
+          <DisplayMoney
+            typography="main21"
+            {...data.totalBorrowsUsd}
             symbolColor="light"
             isFetched={isFetched}
           />
--- src/app/state/ILM/hooks/useFetchIlmPageHeader.tsx
+++ src/app/state/ILM/hooks/useFetchIlmPageHeader.tsx
@@ -1,17 +1,36 @@
-import { loopStrategyAbi } from "../../../generated/generated";
+import { aaveOracleAbi, aaveOracleAddress } from "../../../generated/generated";
 import {
   formatToDisplayable,
   formatUnitsToNumber,
 } from "../../../../shared/utils/helpers";
-import { ilmStrategies } from "../../loop-strategy/config/StrategyConfig";
 import { useReadContracts } from "wagmi";
+import { baseAssets } from "../../lending-borrowing/config/BaseAssetsConfig";
+import { erc20Abi } from "viem";
 
-function useFetchTotalMarketSize() {
-  const multicallParams = ilmStrategies.map((strategy) => ({
-    address: strategy.address,
-    abi: loopStrategyAbi,
-    functionName: "collateral",
-  }));
+function useFetchLendingPoolInfo() {
+  const multicallParams = baseAssets.flatMap((asset) => [
+    {
+      address: asset.sTokenAddress,
+      abi: erc20Abi,
+      functionName: "totalSupply",
+    },
+    {
+      address: asset.debtTokenAddress,
+      abi: erc20Abi,
+      functionName: "totalSupply",
+    },
+    {
+      address: aaveOracleAddress,
+      abi: aaveOracleAbi,
+      functionName: "getAssetPrice",
+      args: [asset.address],
+    },
+    {
+      address: asset.address,
+      abi: erc20Abi,
+      functionName: "decimals",
+    },
+  ]);
 
   const {
     data: results,
@@ -21,31 +40,60 @@ function useFetchTotalMarketSize() {
     contracts: multicallParams,
   });
 
-  let totalMarketSize = 0n;
+  let totalSuppliedUsd = 0n,
+    totalBorrowedUsd = 0n;
   if (results) {
-    for (const result of results) {
-      if (!result.error) {
-        totalMarketSize = totalMarketSize + ((result.result as bigint) || 0n);
-      }
+    for (let i = 0; i < results.length; i += 4) {
+      const totalSupplied = BigInt(results[i].result || 0);
+      const totalBorrowed = BigInt(results[i + 1].result || 0);
+      const assetPrice = BigInt(results[i + 2].result || 0);
+      const assetDecimals = Number(results[i + 3].result || 0);
+
+      totalSuppliedUsd += assetPrice
+        ? (totalSupplied * assetPrice) / BigInt(10 ** assetDecimals)
+        : 0n;
+
+      totalBorrowedUsd += assetPrice
+        ? (totalBorrowed * assetPrice) / BigInt(10 ** assetDecimals)
+        : 0n;
     }
   }
 
   return {
     isLoading,
     isFetched,
-    collateralUSD: formatUnitsToNumber(totalMarketSize, 8),
+    totalMarketSizeUsd: formatUnitsToNumber(totalSuppliedUsd, 8),
+    totalAvailableUsd: formatUnitsToNumber(
+      totalSuppliedUsd - totalBorrowedUsd,
+      8
+    ),
+    totalBorrowsUsd: formatUnitsToNumber(totalBorrowedUsd, 8),
   };
 }
 
 export const useFetchIlmHeaderInfo = () => {
-  const { collateralUSD, isLoading, isFetched } = useFetchTotalMarketSize();
+  const {
+    isLoading,
+    isFetched,
+    totalMarketSizeUsd,
+    totalAvailableUsd,
+    totalBorrowsUsd,
+  } = useFetchLendingPoolInfo();
 
   return {
     isLoading,
     isFetched,
     data: {
-      totalMarketSize: {
-        value: formatToDisplayable(collateralUSD),
+      totalMarketSizeUsd: {
+        value: formatToDisplayable(totalMarketSizeUsd),
+        symbol: "$",
+      },
+      totalAvailableUsd: {
+        value: formatToDisplayable(totalAvailableUsd),
+        symbol: "$",
+      },
+      totalBorrowsUsd: {
+        value: formatToDisplayable(totalBorrowsUsd),
         symbol: "$",
       },
     },
