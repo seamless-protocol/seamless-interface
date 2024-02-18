44331d0f9160409c3a334581a43924246adcae9a
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-18 22:41:14 +0100

    removed clg

--- src/shared/components/wallet/ConnectWalletRainbowWrapper/components/sections/NetworkSection.tsx
+++ src/shared/components/wallet/ConnectWalletRainbowWrapper/components/sections/NetworkSection.tsx
@@ -7,7 +7,6 @@ import { ChainName } from "../ChainName";
 export const NetworkSection: React.FC<{
   chainName?: string | undefined;
 }> = ({ chainName }) => {
-  console.log({ chainName });
   return (
     <FlexCol className="p-2 px-4 gap-2 md:text-primary-dark text-primary-contrast">
       <Typography type="subheader2">Network</Typography>
