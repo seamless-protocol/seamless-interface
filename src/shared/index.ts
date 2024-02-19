122d2a963ca4ee78e900c6e356f5d5c793f48400
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-19 22:37:09 +0100

    removed autoconnect

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -5,11 +5,6 @@ export * from "./contexts/notification/NotificationContext";
 export * from "./contexts/notification/useNotificationContext";
 
 // ****** //
-// hooks //
-// ****** //
-export * from "./hooks/useAutoConnect";
-
-// ****** //
 // auth //
 // ****** //
 export * from "./components/auth/AuthGuard";
