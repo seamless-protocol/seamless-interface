28a825656b3626e3406c19e454998014dc80883b
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-19 22:48:08 +0100

    refactored wallet.config.ts

--- wallet.config.ts
+++ wallet.config.ts
@@ -1,16 +1,11 @@
 import { Chain, http } from "viem";
 import { base } from "wagmi/chains";
-import { injected, safe } from "wagmi/connectors";
 
 export type WalletConfig = {
   appName: string;
 
   targetNetwork: Chain; // make this array?
   walletConnectProjectId: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  connectors: any[]; //todo
-  onlyLocalBurnerWallet: boolean;
-  walletAutoConnect: boolean;
 
   chains: Chain[];
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,16 +14,8 @@ export type WalletConfig = {
 
 export const walletConfig = {
   appName: "Seamless protocol",
-
   targetNetwork: base,
-  connectors: [injected(), safe()],
-
-  walletConnectProjectId:
-    import.meta.env.VITE_BASE_WALLET_PROJECT_ID ||
-    "3a8170812b534d0ff9d794f19a901d64",
-
-  onlyLocalBurnerWallet: true,
-  walletAutoConnect: true,
+  walletConnectProjectId: import.meta.env.VITE_BASE_WALLET_PROJECT_ID || "",
 
   chains: [base],
   transports: {
