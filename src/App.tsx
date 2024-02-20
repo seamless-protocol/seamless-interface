aaf11c305caa726c0c646be59e9f6d3eee390341
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-20 21:18:27 +0100

    removed comments from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { WagmiProvider } from "wagmi"; //, http, createConfig
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { injected, safe } from "wagmi/connectors";
-// import { injected, safe } from "wagmi/connectors";
 import { IlmPage } from "./app/pages/ilm-page/page";
 import { RouterConfig } from "./app/router";
 import { IlmDetailsPage } from "./app/pages/ilm-details-page/page";
