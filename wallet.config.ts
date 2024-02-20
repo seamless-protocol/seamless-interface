53c9ae0c5dc8fd3ba6adcfdd58b8bdeedee502b8
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-20 12:40:06 +0100

    updated wallet config

--- wallet.config.ts
+++ wallet.config.ts
@@ -18,11 +18,9 @@ export type WalletConfig = {
 };
 
 export const walletConfig = {
-  appName: "Seamless protocol",
-  appDescription: `Seamless Protocol is the first native, decentralized, non-custodial lending and borrowing protocol on the Base network. 
-    In the first month of being live, it has grown to be the largest native liquidity market (Top 20 TVL on Base, according to DefiLlama), enabling users to supply and borrow USDbC, ETH, and cbETH (with more assets underway). 
-    Seamless enables users to engage in a new form of peer-to-peer borrowing/lending—Integrated Liquidity Markets (or ILMs, for short)—which are isolated, smart contract-to-smart contract markets for undercollateralized, yet still permissionless, borrowing.`,
-  appUrl: "https://pre-interface.seamlessprotocol.com/",
+  appName: "Seamless Protocol",
+  appDescription: `Seamless Protocol is the first decentralized, native lending and borrowing protocol on Base.`,
+  appUrl: "https://app.seamlessprotocol.com/",
   appIcon: logoSeamless,
   targetNetwork: base,
   walletConnectProjectId: import.meta.env.VITE_BASE_WALLET_PROJECT_ID || "",
