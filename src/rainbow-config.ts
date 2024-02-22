45bd675fb424e5b0fbd47dc0711bcac06a8329a0
kitanovicd <kitanovic.danilo@outlook.com>
2024-02-22 22:21:51 +0100

    fixes

--- src/rainbow-config.ts
+++ src/rainbow-config.ts
@@ -1,7 +1,7 @@
 import { base } from "wagmi/chains";
 import logoSeamless from "@assets/logos/logo-seamless.svg";
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { fallback, http, webSocket } from "wagmi";
+import { fallback, http } from "wagmi";
 
 export const rainbowConfig = getDefaultConfig({
   appName: "Seamless Protocol",
@@ -16,13 +16,13 @@ export const rainbowConfig = getDefaultConfig({
       [
         http(import.meta.env.VITE_BASE_RPC_FREE_1),
         //paid
-        http(import.meta.env.VITE_BASE_MAIN_RPC_URL),
-        webSocket(import.meta.env.VITE_BASE_RPC_PAID_WS_2),
-        //free
-        http(import.meta.env.VITE_BASE_RPC_FREE_2),
-        http(import.meta.env.VITE_BASE_RPC_FREE_3),
-        http(import.meta.env.VITE_BASE_RPC_FREE_4),
-        webSocket(import.meta.env.VITE_BASE_RPC_FREE_WS_5),
+        //   http(import.meta.env.VITE_BASE_MAIN_RPC_URL),
+        //   webSocket(import.meta.env.VITE_BASE_RPC_PAID_WS_2),
+        //   //free
+        //   http(import.meta.env.VITE_BASE_RPC_FREE_2),
+        //   http(import.meta.env.VITE_BASE_RPC_FREE_3),
+        //   http(import.meta.env.VITE_BASE_RPC_FREE_4),
+        //   webSocket(import.meta.env.VITE_BASE_RPC_FREE_WS_5),
       ],
       { rank: true }
     ),
