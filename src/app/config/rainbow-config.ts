302a9f61ea7512af9a6efc40150696f33f4f0868
kitanovicd <kitanovic.danilo@outlook.com>
2024-02-23 15:18:16 +0100

    fix: uncomment rainbow config

--- src/app/config/rainbow-config.ts
+++ src/app/config/rainbow-config.ts
@@ -5,14 +5,14 @@ import { fallback, http, webSocket } from "wagmi";
 
 const rpcConfig = [
   { url: import.meta.env.VITE_BASE_RPC_FREE_1, isWebSocket: false },
-  // Paid
-  //   { url: import.meta.env.VITE_BASE_MAIN_RPC_URL, isWebSocket: false },
-  //   { url: import.meta.env.VITE_BASE_RPC_PAID_WS_2, isWebSocket: true },
-  //   // Free
-  //   { url: import.meta.env.VITE_BASE_RPC_FREE_2, isWebSocket: false },
-  //   { url: import.meta.env.VITE_BASE_RPC_FREE_3, isWebSocket: false },
-  //   { url: import.meta.env.VITE_BASE_RPC_FREE_4, isWebSocket: false },
-  //   { url: import.meta.env.VITE_BASE_RPC_FREE_WS_5, isWebSocket: true },
+  //  Paid
+  { url: import.meta.env.VITE_BASE_MAIN_RPC_URL, isWebSocket: false },
+  { url: import.meta.env.VITE_BASE_RPC_PAID_WS_2, isWebSocket: true },
+  // Free
+  { url: import.meta.env.VITE_BASE_RPC_FREE_2, isWebSocket: false },
+  { url: import.meta.env.VITE_BASE_RPC_FREE_3, isWebSocket: false },
+  { url: import.meta.env.VITE_BASE_RPC_FREE_4, isWebSocket: false },
+  { url: import.meta.env.VITE_BASE_RPC_FREE_WS_5, isWebSocket: true },
 ].filter(({ url }) => url);
 
 export const rainbowConfig = getDefaultConfig({
