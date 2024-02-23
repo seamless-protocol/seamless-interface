51dd572ab43194724d73cf7f2f87dff0aa266fd4
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-23 23:10:52 +0100

    test5

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,6 +1,6 @@
 import { baseAssets } from "../state/lending-borrowing/config/BaseAssetsConfig";
 
-const baseUrl = "#";
+const baseUrl = "";
 const lendingAndBorrowingUrl = "https://legacy.seamlessprotocol.com";
 const stakingFarmsUrl = "https://farms.seamlessprotocol.com";
 const governanceUrl = "https://legacy.seamlessprotocol.com/governance";
@@ -19,11 +19,11 @@ export const RouterConfig = {
     claimAirdrop: `${claimAirdropUrl}`,
     faq: `${faqUrl}`,
     developers: `${developersUrl}`,
-    ilmDetails: `${baseUrl}/ilm-details/:id`,
+    ilmDetails: `${baseUrl}#/ilm-details/:id`,
   },
   // This is used everywhere in app, for navigation and links to build dynamic routes
   Builder: {
-    ilmDetails: (id: number) => `${baseUrl}/ilm-details/${id}`,
+    ilmDetails: (id: number) => `${baseUrl}#/ilm-details/${id}`,
     assetDetails: (id: number) =>
       `${lendingAndBorrowingUrl}/reserve-overview/?underlyingAsset=${baseAssets[id].address.toLowerCase()}&marketName=proto_base_v3`,
     baseScan: (txHash: string) => `${baseScan}/${txHash}`,
