79973901b5fcbc6b969904abe52aa91103adb754
Srdjan Rakic <bans.cowboy@gmail.com>
2024-02-27 00:05:34 +0100

    fix: moved other before classname

--- src/shared/components/wallet/connect-wallet-rainbow-wrapper/components/ConnectButton.tsx
+++ src/shared/components/wallet/connect-wallet-rainbow-wrapper/components/ConnectButton.tsx
@@ -9,10 +9,10 @@ export const ConnectButtonStyled: React.FC<ButtonProps> = ({
 }) => {
   return (
     <button
+      {...other}
       className={`box-border  border-solid border-thin 
   border-other-borderButton text-white bg-primary-main hover:bg-background-header
     rounded px-3 py-1.5 transition-all duration-250 ease-in-out ${className}`}
-      {...other}
     >
       {children}
     </button>
