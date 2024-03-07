5bc55552bea6c38d0f81e2b4052b24be78a2de81
Srdjan Rakic <bans.cowboy@gmail.com>
2024-03-07 19:28:09 +0100

    implemented error mapping and fallback logic

--- src/shared/utils/errorPersers.ts
+++ src/shared/utils/errorPersers.ts
@@ -1,24 +1,49 @@
-import { BaseError } from "viem";
+interface CustomError {
+  details?: string;
+  shortMessage?: string;
+  cause?: {
+    data?: {
+      errorName?: string;
+    };
+    name?:
+      | "ContractFunctionZeroDataError"
+      | "ContractFunctionRevertedError"
+      | string;
+  };
+}
+
 /**
  * @dev utility function to parse error
  * @param e - error object
  * @returns {string} parsed error string
  */
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getParsedError = (e: any | BaseError): string => {
-  let message = e.message ?? "An unknown error occurred";
+export const getParsedError = (e: any | CustomError): string => {
+  let message = e.shortMessage ?? "An unknown error occurred";
 
-  if (e instanceof BaseError) {
-    if (e.details) {
-      message = e.details;
-    } else if (e.shortMessage) {
-      message = e.shortMessage;
-    } else if (e.message) {
-      message = e.message;
-    } else if (e.name) {
-      message = e.name;
+  const error = e as CustomError;
+  if (error) {
+    if (e.cause?.data?.errorName && errorMapping[e.cause?.data?.errorName]) {
+      message = errorMapping[e.cause?.data?.errorName];
+    } else if (e.cause?.name && errorMapping[e.cause?.name]) {
+      message = errorMapping[e.cause?.name];
+    } else if (error.details) {
+      message = error.details;
+    } else if (error.shortMessage) {
+      message = error.shortMessage;
+    } else {
+      message =
+        "Unknown error, feel free to report this to our discord channel.";
     }
   }
 
   return message;
+};
+
+export const errorMapping: Record<string, string> = {
+  ContractFunctionZeroDataError:
+    "Contract not found! Make sure you are on correct chain.",
+  EnforcedPause: "Temporary pause in effect, please check Discord for updates.",
+  ErrorNotEnoughAllowance:
+    "Not enough allowance, did you approve your tokens first?",
 };
