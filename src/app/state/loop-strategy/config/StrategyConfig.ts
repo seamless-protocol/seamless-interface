1f731436a3a83eb7f1fc795e60d246426037361c
Srdjan Rakic <bans.cowboy@gmail.com>
2024-03-07 19:27:53 +0100

    added conditional address, for tenderly and base mainnet

--- src/app/state/loop-strategy/config/StrategyConfig.ts
+++ src/app/state/loop-strategy/config/StrategyConfig.ts
@@ -5,6 +5,7 @@ import EthLogo from "@assets/tokens/eth.svg";
 import WstEthLogo from "@assets/tokens/wsteth.svg";
 import wstEthDiagram from "@assets/wsteth-diagram.png";
 import { WSTETH_ADDRESS, WETH_ADDRESS } from "@meta";
+const RPC_URL: string = import.meta.env.VITE_BASE_RPC_FREE_1;
 
 interface AssetConfig {
   name: string;
@@ -28,8 +29,9 @@ export const ilmStrategies: StrategyConfig[] = [
   {
     name: "wstETH Booster",
     symbol: "ilmwstETH",
-    //0x2962673cC60eE877768A38fa6d7FEe7468b3F09b
-    address: "0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e",
+    address: RPC_URL.startsWith("https://rpc.tenderly.co")
+      ? "0x2962673cC60eE877768A38fa6d7FEe7468b3F09b" //use tenderly address
+      : "0x258730e23cF2f25887Cb962d32Bd10b878ea8a4e", //use basenet address
     logo: ilmwstETHLogo,
     diagram: wstEthDiagram,
     defaultApy: 7.74,
