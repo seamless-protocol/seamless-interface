61e73bcfbd02f7e556274c1aea8c61b77d296be7
Rakic <bans.cowboy@gmail.com>
2024-03-13 17:24:29 +0100

    upgraded tooltip component

--- src/shared/components/tooltip/Tooltip.tsx
+++ src/shared/components/tooltip/Tooltip.tsx
@@ -1,6 +1,9 @@
 import React, { useId } from "react";
 import { Tooltip as ReactTooltip } from "react-tooltip";
 
+/**
+ * Determines the placement of the tooltip.
+ */
 export type TooltipPlace =
   | "top"
   | "top-start"
@@ -15,13 +18,64 @@ export type TooltipPlace =
   | "left-start"
   | "left-end";
 
+/**
+ * Theme options for the tooltip's appearance.
+ */
+export type TooltipTheme = "light" | "dark";
+
+/**
+ * Size options for the tooltip, affecting its padding.
+ */
+export type TooltipSize = "small" | "normal" | "big";
+
+/**
+ * Props for the Tooltip component.
+ * @param children - The trigger element for the tooltip.
+ * @param tooltip - The content to be displayed within the tooltip.
+ * @param place - Optional. The preferred placement of the tooltip.
+ * @param openOnClick - Optional. Determines if the tooltip should open on click.
+ * @param theme - Optional. The theme of the tooltip, affecting its background and text color.
+ * @param size - Optional. The size of the tooltip, affecting its padding.
+ */
 export const Tooltip: React.FC<{
   children: React.ReactElement;
   tooltip: React.ReactNode;
   place?: TooltipPlace;
   openOnClick?: boolean;
-}> = ({ children, tooltip, openOnClick, place = "top" }) => {
+  theme?: TooltipTheme;
+  size?: TooltipSize;
+}> = ({
+  children,
+  tooltip,
+  openOnClick,
+  place = "top",
+  theme = "light",
+  size = "normal",
+}) => {
   const tooltipId = useId();
+
+  const themeStyles = {
+    light: {
+      backgroundColor: "white",
+      color: "black",
+    },
+    dark: {
+      backgroundColor: "rgba(0, 0, 0, 0.65)",
+      color: "white",
+    },
+  };
+
+  const sizeStyles = {
+    small: {
+      padding: "4px 8px",
+    },
+    normal: {
+      padding: "16px 24px",
+    },
+    big: {
+      padding: "24px 32px",
+    },
+  };
 
   const clonedChild = React.cloneElement(children, {
     "data-tooltip-id": tooltipId,
@@ -32,9 +86,8 @@ export const Tooltip: React.FC<{
       {clonedChild}
       <ReactTooltip
         style={{
-          backgroundColor: "white",
-          color: "black",
-          padding: "16px 24px",
+          ...themeStyles[theme],
+          ...sizeStyles[size],
           zIndex: 99999999,
           boxShadow:
             "rgba(0, 0, 0, 0.2) 0px 0px 2px, rgba(0, 0, 0, 0.1) 0px 2px 10px",
