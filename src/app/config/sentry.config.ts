28d7c65bd08b322d87389e6e266664d60b2fb7e5
Rakic <bans.cowboy@gmail.com>
2024-03-13 14:16:07 +0100

    updated comments

--- src/app/config/sentry.config.ts
+++ src/app/config/sentry.config.ts
@@ -37,7 +37,7 @@ if (!IS_DEV_MODE) {
       /^https:\/\/app\.seamlessprotocol\.com\/.*/,
     ],
     // Session Replay
-    replaysSessionSampleRate: 0.0, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysSessionSampleRate: 0.0, // This sets the sample rate at 0%.
+    replaysOnErrorSampleRate: 1.0, // Monitoring 100% sessions where error occur.
   });
 }
