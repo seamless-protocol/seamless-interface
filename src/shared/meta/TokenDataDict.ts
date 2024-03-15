d32f49845a200889053d883d68caef55cf94e3ed
Rakic <bans.cowboy@gmail.com>
2024-03-15 16:17:58 +0100

    added tokendata dictionary

---
+++ src/shared/meta/TokenDataDict.ts
@@ -0,0 +1,36 @@
+import { WETH_ADDRESS, WSTETH_ADDRESS, wstETHBooster_ADDRESS } from ".";
+import { Address } from "viem";
+
+import wethLogo from "@assets/tokens/weth.svg";
+import WstEthLogo from "@assets/tokens/wsteth.svg";
+import ilmwstETHLogo from "@assets/tokens/ilmwstETH.svg";
+
+interface ITokenDataDict {
+  [address: Address]: {
+    name: string;
+    shortName?: string;
+    logo?: string;
+    symbol?: string;
+    decimals?: number;
+  };
+}
+
+export const TokenDataDict: ITokenDataDict = {
+  [WETH_ADDRESS]: {
+    name: "Wrapped Ethereum",
+    shortName: "WETH",
+    symbol: "WETH",
+    logo: wethLogo,
+    decimals: 18,
+  },
+  [WSTETH_ADDRESS]: {
+    name: "Wrapped liquid staked ETH",
+    symbol: "wstETH",
+    logo: WstEthLogo,
+  },
+  [wstETHBooster_ADDRESS]: {
+    name: "wstETH Booster",
+    symbol: "ilmwstETH",
+    logo: ilmwstETHLogo,
+  },
+};
