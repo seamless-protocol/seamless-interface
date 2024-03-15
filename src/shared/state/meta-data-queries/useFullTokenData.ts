7e5f21fb359f5315b8b47ceab1b58f3a7211261f
Rakic <bans.cowboy@gmail.com>
2024-03-15 16:17:45 +0100

    added new hook useFullTokenData

---
+++ src/shared/state/meta-data-queries/useFullTokenData.ts
@@ -0,0 +1,52 @@
+import { Address, erc20Abi } from "viem";
+import { FetchData } from "../../types/Fetch";
+import { useSeamlessContractRead } from "../../wagmi-wrapper/hooks/useSeamlessContractRead";
+import { TokenDataDict } from "../../meta/TokenDataDict";
+
+export interface FullTokenData {
+  symbol: string;
+  decimals: number;
+  logo?: string;
+  name?: string;
+  shortName?: string;
+}
+
+export const useFullTokenData = (token: Address): FetchData<FullTokenData> => {
+  const data = TokenDataDict[token];
+
+  const {
+    data: decimals,
+    isLoading: isDecimalsLoading,
+    isFetched: isDecimalsFetched,
+  } = useSeamlessContractRead({
+    address: token,
+    abi: erc20Abi,
+    functionName: "decimals",
+    query: {
+      staleTime: Infinity,
+    },
+  });
+
+  const {
+    data: symbol,
+    isLoading: isSymbolLoading,
+    isFetched: isSymbolFetched,
+  } = useSeamlessContractRead({
+    address: token,
+    abi: erc20Abi,
+    functionName: "symbol",
+    query: {
+      staleTime: Infinity,
+    },
+  });
+
+  return {
+    isLoading: isDecimalsLoading || isSymbolLoading,
+    isFetched: isDecimalsFetched && isSymbolFetched,
+    data: {
+      ...data,
+      symbol: symbol || data.symbol || "",
+      decimals: decimals || data.decimals || 18,
+    },
+  };
+};
