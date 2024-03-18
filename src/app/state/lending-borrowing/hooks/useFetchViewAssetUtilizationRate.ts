c33ae669efcccfb0be6f08be76cb6ee31c6c268a
kitanovicd <kitanovic.danilo@outlook.com>
2024-03-18 16:46:10 +0100

    fix: remove view hooks from view hooks

--- src/app/state/lending-borrowing/hooks/useFetchViewAssetUtilizationRate.ts
+++ src/app/state/lending-borrowing/hooks/useFetchViewAssetUtilizationRate.ts
@@ -1,39 +1,29 @@
 import { Address } from "viem";
-import { useFetchViewDetailTotalSupplied } from "./useFetchViewDetailTotalSupplied";
-import { useFetchViewDetailTotalBorrowed } from "./useFetchViewDetailTotalBorrowed";
 import { Displayable, ViewBigInt } from "../../../../shared";
 import { formatFetchBigIntToViewBigInt } from "../../../../shared/utils/helpers";
 import { FetchBigInt, FetchData } from "../../../../shared/types/Fetch";
 import { ONE_ETHER } from "../../../meta";
+import { useFetchReserveData } from "../queries/useFetchReserveData";
 
 export const useFetchAssetUtilizationRate = (
   asset: Address
 ): FetchData<FetchBigInt> => {
   const {
-    isLoading: isTotalSuppliedLoading,
-    isFetched: isTotalSuppliedFetched,
-    data: { totalSupplied },
-  } = useFetchViewDetailTotalSupplied(asset);
-
-  const {
-    isLoading: isTotalBorrowedLoading,
-    isFetched: isTotalBorrowedFetched,
-    data: { totalBorrowed },
-  } = useFetchViewDetailTotalBorrowed(asset);
+    isLoading,
+    isFetched,
+    data: { totalSupplied, totalBorrowed },
+  } = useFetchReserveData(asset);
 
   let utilizationRate;
-  if (
-    totalSupplied.tokenAmount.bigIntValue &&
-    totalBorrowed.tokenAmount.bigIntValue
-  ) {
+  if (totalSupplied.bigIntValue && totalBorrowed.bigIntValue) {
     utilizationRate =
-      (totalBorrowed.tokenAmount.bigIntValue * ONE_ETHER * 100n) /
-      totalSupplied.tokenAmount.bigIntValue;
+      (totalBorrowed.bigIntValue * ONE_ETHER * 100n) /
+      totalSupplied.bigIntValue;
   }
 
   return {
-    isLoading: isTotalSuppliedLoading || isTotalBorrowedLoading,
-    isFetched: isTotalSuppliedFetched && isTotalBorrowedFetched,
+    isLoading,
+    isFetched,
     data: {
       bigIntValue: utilizationRate || 0n,
       decimals: 18,
