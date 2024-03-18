8a834fa17aaf9b8dff833a54f46fa4afa80898d6
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2024-03-18 18:08:48 +0100

    feat: fetch lending and borrowing data from old UI (#200)
    Implemented hooks for supply and borrow caps, ltv, liquidation threshold, liquidation penalty, reserve factor and utilization rate

---
+++ src/app/state/lending-borrowing/hooks/useFetchViewDetailReserveCaps.ts
@@ -0,0 +1,79 @@
+import { Address } from "viem";
+import { Displayable } from "../../../../shared";
+import { FetchBigInt, FetchData } from "../../../../shared/types/Fetch";
+import { formatFetchBigIntToViewBigInt } from "../../../../shared/utils/helpers";
+import { useFetchReserveCaps } from "../queries/useFetchViewReserveCaps";
+import { useFetchAssetPrice } from "../../common/queries/useFetchViewAssetPrice";
+import { ViewDetailReserveCaps } from "../types/ViewDetailReserveCaps";
+
+interface DetailReserveCaps {
+  supplyCap: FetchBigInt;
+  supplyCapUsd: FetchBigInt;
+  borrowCap: FetchBigInt;
+  borrowCapUsd: FetchBigInt;
+}
+
+export const useFetchDetailReserveCaps = (
+  asset: Address
+): FetchData<DetailReserveCaps> => {
+  const {
+    isLoading: isCapsLoading,
+    isFetched: isCapsFetched,
+    data: { supplyCap, borrowCap },
+  } = useFetchReserveCaps(asset);
+
+  let {
+    isLoading: isPriceLoading,
+    isFetched: isPriceFetched,
+    data: price,
+  } = useFetchAssetPrice(asset);
+  price = price || 0n;
+
+  return {
+    isLoading: isCapsLoading || isPriceLoading,
+    isFetched: isCapsFetched && isPriceFetched,
+    data: {
+      supplyCap,
+      supplyCapUsd: {
+        bigIntValue:
+          (supplyCap.bigIntValue * price.bigIntValue) /
+          BigInt(10 ** supplyCap.decimals),
+        symbol: price.symbol,
+        decimals: price.decimals,
+      },
+      borrowCap,
+      borrowCapUsd: {
+        bigIntValue:
+          (borrowCap.bigIntValue * price.bigIntValue) /
+          BigInt(10 ** borrowCap.decimals),
+        symbol: price.symbol,
+        decimals: price.decimals,
+      },
+    },
+  };
+};
+
+export const useFetchViewDetailReserveCaps = (
+  asset: Address
+): Displayable<ViewDetailReserveCaps> => {
+  const {
+    isLoading,
+    isFetched,
+    data: { supplyCap, supplyCapUsd, borrowCap, borrowCapUsd },
+  } = useFetchDetailReserveCaps(asset);
+
+  return {
+    isLoading,
+    isFetched,
+    data: {
+      supplyCap: {
+        tokenAmount: formatFetchBigIntToViewBigInt(supplyCap),
+        dollarAmount: formatFetchBigIntToViewBigInt(supplyCapUsd),
+      },
+      borrowCap: {
+        tokenAmount: formatFetchBigIntToViewBigInt(borrowCap),
+        dollarAmount: formatFetchBigIntToViewBigInt(borrowCapUsd),
+      },
+    },
+  };
+};
