4050c273ced507995ca51c9a08a3267132039e88
Srdjan Rakic <bans.cowboy@gmail.com>
2024-03-21 11:38:28 +0100

    returned v1 tailwind exported file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-// import config from "./tailwind.config.v1";
-import config from "./tailwind.config.v2";
+import config from "./tailwind.config.v1";
+// import config from "./tailwind.config.v2";
 
 module.exports = config;
