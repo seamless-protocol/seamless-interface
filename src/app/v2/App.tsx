a5a8d77d6f516fead884d4fb1309d0ed05e3e275
Srdjan Rakic <bans.cowboy@gmail.com>
2024-03-25 20:38:52 +0100

    removed test code

--- src/app/v2/App.tsx
+++ src/app/v2/App.tsx
@@ -10,17 +10,9 @@ import { FallbackPage, FlexCol, PageNotFound } from "@shared";
 //** SENTRY **/
 import * as Sentry from "@sentry/react";
 import { TestPage } from "./pages/TestPage";
-import { useEffect } from "react";
 const SentryRoutes = Sentry.withSentryReactRouterV6Routing(Routes);
 
 export function App() {
-  useEffect(() => {
-    // Read the environment variable
-    const styleVersion = import.meta.env.VITE_STYLE_VERSION;
-    const themeClass = styleVersion === "v2" ? "lightv2" : "light";
-    document.body.classList.add(themeClass);
-  }, []);
-
   return (
     <Sentry.ErrorBoundary fallback={FallbackPage} showDialog>
       <HashRouter>
