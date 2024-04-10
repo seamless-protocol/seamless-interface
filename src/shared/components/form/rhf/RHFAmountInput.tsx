90319b1c19361629d07241d1afc763675e3b5d5e
Srdjan Rakic <bans.cowboy@gmail.com>
2024-04-10 19:45:36 +0200

    updated logged out state of input field

--- src/shared/components/form/rhf/RHFAmountInput.tsx
+++ src/shared/components/form/rhf/RHFAmountInput.tsx
@@ -1,6 +1,6 @@
 import { useFormContext } from "react-hook-form";
 import { Displayable, ViewBigInt } from "src/shared/types/Displayable";
-import { Address, formatUnits } from "viem";
+import { Address, formatUnits, parseUnits } from "viem";
 import { FlexCol } from "../../containers/FlexCol";
 import { FlexRow } from "../../containers/FlexRow";
 import { Icon } from "../../images/Icon";
@@ -10,6 +10,8 @@ import { Typography } from "../../text/Typography/Typography";
 import { DisplayMoney } from "../../display/DisplayMoney";
 import { DisplayTokenAmount } from "../../display/DisplayTokenAmount";
 import { useEffect } from "react";
+import { useAccount } from "wagmi";
+import { MAX_NUMBER } from "../../../../globals";
 
 export interface IRHFAmountInputProps<T> extends RHFInputFieldProps<T> {
   assetAddress: Address;
@@ -26,7 +28,8 @@ export function RHFAmountInput<T>({
   assetButton,
   ...other
 }: IRHFAmountInputProps<T>) {
-  const { setValue } = useFormContext();
+  const { setValue, getValues } = useFormContext();
+  const { isConnected } = useAccount();
   const { data: tokenData } = useFullTokenData(assetAddress);
 
   const handleMaxClick = () => {
@@ -43,12 +46,31 @@ export function RHFAmountInput<T>({
     setValue(name as string, "");
   }, [assetAddress]);
 
+  useEffect(() => {
+    const value = getValues(name as string);
+
+    if (!tokenData?.decimals) {
+      setValue(name as string, "");
+    } else if ((isConnected && (walletBalance?.data?.bigIntValue || 0n) < parseUnits(value, tokenData.decimals)) || 0n)
+      setValue(name as string, "");
+  }, [isConnected]);
+
   return (
     <div className="border bg-neutral-0 rounded-2xl p-4">
       <FlexRow className="items-center w-full">
         <FlexCol className="flex-grow gap-2 text-medium4">
-          <RHFInputField<T> name={name} min={0} max={walletBalance?.data?.value || "0"} placeholder="0.00" {...other} />
-          <DisplayMoney {...dollarValue} {...dollarValue?.data} typography="medium2" />
+          <RHFInputField<T>
+            name={name}
+            min={0}
+            max={isConnected ? walletBalance?.data?.value || "0" : String(MAX_NUMBER)}
+            placeholder="0.00"
+            {...other}
+          />
+          {assetAddress ? (
+            <DisplayMoney {...dollarValue} {...dollarValue?.data} typography="medium2" />
+          ) : (
+            <span className="min-h-[18px]" />
+          )}
         </FlexCol>
         <div className="flex flex-col items-end gap-2">
           {assetButton || (
@@ -57,12 +79,15 @@ export function RHFAmountInput<T>({
               <Typography type="medium4">{tokenData?.symbol}</Typography>
             </div>
           )}
-          <div className="inline-flex gap-2 items-center">
-            <DisplayTokenAmount {...walletBalance} {...walletBalance?.data} typography="medium2" />
-            <button type="button" onClick={handleMaxClick}>
-              <Typography type="bold2">MAX</Typography>
-            </button>
-          </div>
+          {isConnected && (
+            <div className="inline-flex gap-2 items-center">
+              <DisplayTokenAmount {...walletBalance} {...walletBalance?.data} typography="medium2" />
+              <button type="button" onClick={handleMaxClick}>
+                <Typography type="bold2">MAX</Typography>
+              </button>
+            </div>
+          )}
+          {!isConnected && <span className="min-h-[18px]" />}
         </div>
       </FlexRow>
     </div>
