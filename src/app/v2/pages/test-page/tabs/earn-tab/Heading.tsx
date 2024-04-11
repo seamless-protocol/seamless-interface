7608ab53b54d9babdd60bbe4dd7d47426965ea74
kitanovicd <kitanovic.danilo@outlook.com>
2024-04-11 22:58:16 +0200

    fixes

--- src/app/v2/pages/test-page/tabs/earn-tab/Heading.tsx
+++ src/app/v2/pages/test-page/tabs/earn-tab/Heading.tsx
@@ -22,29 +22,29 @@ export const Heading = () => {
   } = useFetchViewAssetPrice(asset);
 
   return (
-    asset && (
-      <div className="grid grid-cols-12 gap-6">
-        <div className="col-span-5">
-          <FlexCol className="gap-3">
-            <FlexCol className="gap-2 min-h-20">
-              <Typography type="bold5">{tokenData.name || "Choose your strategy to earn APY"}</Typography>
-              <Typography type="regular1">{description}</Typography>
-            </FlexCol>
-            <Modal
-              size="biger"
-              buttonProps={{
-                children: (
-                  <FlexRow className="gap-2">
-                    <Typography type="medium3">Learn more</Typography>
-                    <ArrowRightCircleIcon width={22} />
-                  </FlexRow>
-                ),
-              }}
-            >
-              <img src={diagramPng} alt="ilmDiagram" />
-            </Modal>
+    <div className="grid grid-cols-12 gap-6">
+      <div className="col-span-5">
+        <FlexCol className="gap-3">
+          <FlexCol className="gap-2 min-h-20">
+            <Typography type="bold5">{tokenData.name || "Choose your strategy to earn APY"}</Typography>
+            <Typography type="regular1">{description}</Typography>
           </FlexCol>
-        </div>
+          <Modal
+            size="biger"
+            buttonProps={{
+              children: (
+                <FlexRow className="gap-2">
+                  <Typography type="medium3">Learn more</Typography>
+                  <ArrowRightCircleIcon width={22} />
+                </FlexRow>
+              ),
+            }}
+          >
+            <img src={diagramPng} alt="ilmDiagram" />
+          </Modal>
+        </FlexCol>
+      </div>
+      {asset && (
         <div className="col-span-7">
           <FlexRow className="gap-24 justify-center w-full mt-2">
             <FlexCol className="gap-1 text-center">
@@ -66,7 +66,7 @@ export const Heading = () => {
             </FlexCol>
           </FlexRow>
         </div>
-      </div>
-    )
+      )}
+    </div>
   );
 };
