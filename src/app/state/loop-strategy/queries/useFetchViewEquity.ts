cd8b94cb22b4ced0139aeea863744a1e5fb1588f
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2024-04-11 23:05:54 +0200

    feat: earn header (#249)

---
+++ src/app/state/loop-strategy/queries/useFetchViewEquity.ts
@@ -0,0 +1,74 @@
+import { Address } from "viem";
+import { Displayable, formatFetchBigIntToViewBigIntTemp, useSeamlessContractRead, useToken } from "@shared";
+import { FetchBigInt, FetchData } from "../../../../shared/types/Fetch";
+import { useFetchStrategyAsset } from "../metadataQueries/useFetchStrategyAsset";
+import { loopStrategyAbi } from "../../../generated";
+import { ViewDetailEquity } from "../types/ViewDetailEquity";
+
+interface StrategyEquity {
+  equity: FetchBigInt | undefined;
+  equityUsd: FetchBigInt | undefined;
+}
+
+export const useFetchDetailEquity = (strategy: Address): FetchData<StrategyEquity> => {
+  const {
+    data: underlyingAsset,
+    isLoading: isUnderlyingAssetLoading,
+    isFetched: isUnderlyingAssetFetched,
+  } = useFetchStrategyAsset(strategy);
+
+  const {
+    data: { symbol, decimals },
+    isLoading: isTokenDataLoading,
+    isFetched: isTokenDataFetched,
+  } = useToken(underlyingAsset);
+
+  const {
+    data: equity,
+    isLoading: isEquityLoading,
+    isFetched: isEquityFetched,
+  } = useSeamlessContractRead({
+    address: strategy,
+    abi: loopStrategyAbi,
+    functionName: "equity",
+  });
+
+  const {
+    data: equityUsd,
+    isLoading: isEquityUsdLoading,
+    isFetched: isEquityUsdFetched,
+  } = useSeamlessContractRead({
+    address: strategy,
+    abi: loopStrategyAbi,
+    functionName: "equityUSD",
+  });
+
+  const retEquity = equity ? { bigIntValue: equity, decimals, symbol } : undefined;
+  const retEquityUsd = equityUsd ? { bigIntValue: equityUsd, decimals: 8, symbol: "$" } : undefined;
+
+  return {
+    isLoading: isEquityLoading || isEquityUsdLoading || isUnderlyingAssetLoading || isTokenDataLoading,
+    isFetched: isEquityFetched && isEquityUsdFetched && isUnderlyingAssetFetched && isTokenDataFetched,
+    data: {
+      equity: retEquity,
+      equityUsd: retEquityUsd,
+    },
+  };
+};
+
+export const useFetchViewDetailEquity = (strategy: Address): Displayable<ViewDetailEquity> => {
+  const {
+    isLoading,
+    isFetched,
+    data: { equity, equityUsd },
+  } = useFetchDetailEquity(strategy);
+
+  return {
+    isLoading,
+    isFetched,
+    data: {
+      tokenAmount: formatFetchBigIntToViewBigIntTemp(equity),
+      dollarAmount: formatFetchBigIntToViewBigIntTemp(equityUsd),
+    },
+  };
+};
--- src/app/v2/pages/test-page/tabs/earn-tab/Heading.tsx
+++ src/app/v2/pages/test-page/tabs/earn-tab/Heading.tsx
@@ -1,16 +1,25 @@
-import { FlexCol, Typography, FlexRow, Modal, useFullTokenData } from "@shared";
+import { FlexCol, Typography, FlexRow, Modal, useFullTokenData, DisplayMoney } from "@shared";
 
 import { ArrowRightCircleIcon } from "@heroicons/react/24/outline";
 
 import diagramPng from "@assets/wsteth-diagram.svg";
 import { useAssetPickerState } from "../../../../hooks/useAssetPickerState";
+import { useFetchViewAssetPrice } from "../../../../../state/common/queries/useFetchViewAssetPrice";
 import { getTokenDescription } from "../../../../../../shared/state/meta-data-queries/useTokenDescription";
 import { assetSlugConfig } from "./config/SlugConfig";
+import { AssetApy } from "../../../../components/AssetApy";
+import { AssetTvl } from "../../../../components/AssetTvl";
 
 export const Heading = () => {
-  const { asset } = useAssetPickerState({ overrideUrlSlug: assetSlugConfig });
+  const { asset, isStrategy } = useAssetPickerState({ overrideUrlSlug: assetSlugConfig });
   const { data: tokenData } = useFullTokenData(asset);
   const description = getTokenDescription(asset);
+
+  const {
+    data: oraclePrice,
+    isLoading: isOraclePriceLoading,
+    isFetched: isOraclePriceFetched,
+  } = useFetchViewAssetPrice(asset);
 
   return (
     <div className="grid grid-cols-12 gap-6">
@@ -35,22 +44,29 @@ export const Heading = () => {
           </Modal>
         </FlexCol>
       </div>
-      <div className="col-span-7">
-        <FlexRow className="gap-24 justify-center w-full mt-2">
-          <FlexCol className="gap-1 text-center">
-            <Typography type="regular3">TVL</Typography>
-            <Typography type="bold5">XX / XX</Typography>
-          </FlexCol>
-          <FlexCol className="gap-1 text-center">
-            <Typography type="regular3">APY, up to</Typography>
-            <Typography type="bold5">12.32%</Typography>
-          </FlexCol>
-          <FlexCol className="gap-1 text-center">
-            <Typography type="regular3">Oracle price</Typography>
-            <Typography type="bold5">$2.36k</Typography>
-          </FlexCol>
-        </FlexRow>
-      </div>
+      {asset && (
+        <div className="col-span-7">
+          <FlexRow className="gap-24 justify-center w-full mt-2">
+            <FlexCol className="gap-1 text-center">
+              <Typography type="regular3">TVL</Typography>
+              <AssetTvl asset={asset} isStrategy={isStrategy} typography="bold5" />
+            </FlexCol>
+            <FlexCol className="gap-1 text-center">
+              <Typography type="regular3">APY, up to</Typography>
+              <AssetApy asset={asset} isStrategy={isStrategy} typography="bold5" />
+            </FlexCol>
+            <FlexCol className="gap-1 text-center">
+              <Typography type="regular3">Oracle price</Typography>
+              <DisplayMoney
+                typography="bold5"
+                {...oraclePrice}
+                isLoading={isOraclePriceLoading}
+                isFetched={isOraclePriceFetched}
+              />
+            </FlexCol>
+          </FlexRow>
+        </div>
+      )}
     </div>
   );
 };
