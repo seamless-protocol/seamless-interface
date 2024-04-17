43d181e98972a52bd5bf46983963a0af2df16992
Srdjan Rakic <bans.cowboy@gmail.com>
2024-04-17 21:01:18 +0200

    fixed form header

--- src/app/v2/components/forms/SupplyForm/SupplyForm.tsx
+++ src/app/v2/components/forms/SupplyForm/SupplyForm.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useFullTokenData, MyFormProvider, FlexCol, Typography, WatchAssetComponentv2, useNotificationContext } from "../../../../../shared";
+import { useFullTokenData, MyFormProvider, FlexCol, Typography, WatchAssetComponentv2, useNotificationContext, FlexRow } from "../../../../../shared";
 import { RHFAmountInputWrapper } from "../../RHFAmountInputWrapper";
 import { useAssetPickerState } from "../../../hooks/useAssetPickerState";
 import { assetSlugConfig, earnInputConfig } from "../../../pages/test-page/tabs/earn-tab/config/SlugConfig";
@@ -7,6 +7,7 @@ import { SupplyButtons } from "./SupplyButtons";
 import { SupplySummary } from "./SupplySummary";
 import { useMutateSupplyLending } from "../../../../state/lending-borrowing/mutations/useMutateSupplyLending";
 import { DepositModalFormData } from "../../../../v1/pages/ilm-details-page/components/your-info/deposit/DepositModal";
+import { Tag } from "../../../pages/test-page/tabs/earn-tab/Tag";
 
 export const SupplyForm = () => {
   const { asset } = useAssetPickerState({ overrideUrlSlug: assetSlugConfig });
@@ -52,9 +53,17 @@ export const SupplyForm = () => {
   return (
     <MyFormProvider methods={methods} onSubmit={handleSubmit(onSubmitAsync)}>
       <FlexCol className="gap-8">
-        <Typography type="bold4">{asset ? "Supply" : "Select Asset"}</Typography>
-        <Typography type="regular3">{tokenData.name}</Typography>
-        <RHFAmountInputWrapper {...earnInputConfig} />
+        <FlexCol className="gap-6">
+          <FlexRow className="justify-between items-start">
+            <FlexCol className="gap-1 min-h-14">
+              <Typography type="bold4">{asset ? "Supply" : "Select Asset"}</Typography>
+              <Typography type="regular3">{tokenData.name}</Typography>
+            </FlexCol>
+
+            {asset != null && <Tag tag="LEND" />}
+          </FlexRow>
+          <RHFAmountInputWrapper {...earnInputConfig} />
+        </FlexCol>
 
         <SupplySummary asset={asset} />
         <SupplyButtons />
@@ -62,4 +71,3 @@ export const SupplyForm = () => {
     </MyFormProvider>
   );
 };
-
