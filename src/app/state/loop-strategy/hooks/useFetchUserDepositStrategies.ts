ddd2b1229e41697e90f6ed68331f6749fe2de10e
Srdjan Rakic <bans.cowboy@gmail.com>
2024-04-18 19:49:44 +0200

    fixed flashing bug when loading my positions

--- src/app/state/loop-strategy/hooks/useFetchUserDepositStrategies.ts
+++ src/app/state/loop-strategy/hooks/useFetchUserDepositStrategies.ts
@@ -15,40 +15,37 @@ const fetchUserDepositStrategies = async (
   user: Address | undefined
 ): Promise<Strategy[]> => {
   if (!config || !user) return [];
-
-  const strategies: Strategy[] = [];
-
-  // eslint-disable-next-line
-  for (const depositAsset of ilmAssetStrategiesMap.keys()) {
+  const promises = Array.from(ilmAssetStrategiesMap.keys()).map(async (depositAsset) => {
     const strategy = ilmAssetStrategiesMap.get(depositAsset)?.[0];
+    if (!strategy) return undefined;
 
-    if (strategy) {
-      // eslint-disable-next-line
-      const balance = await readContract(config, {
-        address: strategy.address,
-        abi: loopStrategyAbi,
-        functionName: "balanceOf",
-        args: [user],
-      });
+    const balance = await readContract(config, {
+      address: strategy.address,
+      abi: loopStrategyAbi,
+      functionName: "balanceOf",
+      args: [user],
+    });
 
-      if (balance && balance > 0n) {
-        strategies.push({
-          asset: depositAsset,
-          strategy: strategy.address,
-        });
-      }
+    if (balance && balance > 0n) {
+      return {
+        asset: depositAsset,
+        strategy: strategy.address,
+      };
     }
-  }
+    return undefined;
+  });
 
+  const strategies = (await Promise.all(promises)).filter((strategy): strategy is Strategy => strategy !== undefined);
   return strategies;
-};
+}
 
 export const useFetchUserDepositStrategies = () => {
   const config = useConfig();
   const account = useAccount();
 
   return useQuery({
-    queryKey: ["userDepositStrategies", account],
+    queryKey: ["userDepositStrategies", account?.address],
     queryFn: () => fetchUserDepositStrategies(config, account?.address),
+    enabled: !!account?.address && !!config
   });
 };
