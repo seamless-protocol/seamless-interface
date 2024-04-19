779828db30bf2a73bf4d2a3b8f6d5dbf823b59ad
kitanovicd <kitanovic.danilo@outlook.com>
2024-04-19 12:05:15 +0200

    fix: remove APR from strategy form

--- src/app/v2/components/forms/DepositStrategyForm/StrategySummary.tsx
+++ src/app/v2/components/forms/DepositStrategyForm/StrategySummary.tsx
@@ -5,7 +5,6 @@ import { findILMStrategyByAddress, StrategyConfig } from "../../../../state/loop
 import { useFetchViewTargetMultiple } from "../../../../state/loop-strategy/hooks/useFetchViewTargetMultiple";
 import { useAssetPickerState } from "../../../hooks/useAssetPickerState";
 import { assetSlugConfig } from "../../../pages/test-page/tabs/earn-tab/config/SlugConfig";
-import { AssetApr } from "../../AssetApr";
 import { StrategyApy } from "../../AssetApy";
 
 export const StrategySummary: React.FC<{
@@ -25,12 +24,10 @@ export const StrategySummary: React.FC<{
 const StrategySummaryLocal: React.FC<{
   strategy: StrategyConfig;
 }> = ({ strategy }) => {
-
   const { asset } = useAssetPickerState({ overrideUrlSlug: assetSlugConfig });
   const { data: tokenData } = useFullTokenData(asset);
 
   const { data: strategyTokenData } = useFullTokenData(strategy.address);
-
 
   const {
     data: targetMultiple,
@@ -38,24 +35,21 @@ const StrategySummaryLocal: React.FC<{
     isFetched: isTargetMultipleFetched,
   } = useFetchViewTargetMultiple(strategy.address);
 
-
-
   return (
     <FlexCol className="rounded-card bg-neutral-100 p-6 gap-4">
       <Typography type="bold3">Summary</Typography>
       <LocalRow label="Estimated APY">
-        {asset && (
-          <StrategyApy asset={asset} className="text-navy-1000" typography="medium2" />
-        )}
+        {asset && <StrategyApy asset={asset} className="text-navy-1000" typography="medium2" />}
       </LocalRow>
-      <FlexRow className="text-navy-600 justify-between">
-        <Typography type="bold2">Rewards APR</Typography>
-        {asset && <AssetApr asset={asset} className="text-navy-1000" typography="medium2" />}
-      </FlexRow>
       <LocalRow label="Action">Deposit</LocalRow>
       <LocalRow label="Strategy">{TokenDescriptionDict[asset]?.strategyTitle}</LocalRow>
       <LocalRow label="Multiplier">
-        <DisplayValue {...targetMultiple} isLoading={isTargetMultipleLoading} isFetched={isTargetMultipleFetched} loaderSkeleton />
+        <DisplayValue
+          {...targetMultiple}
+          isLoading={isTargetMultipleLoading}
+          isFetched={isTargetMultipleFetched}
+          loaderSkeleton
+        />
       </LocalRow>
       <LocalRow label="Starting Asset">
         <FlexRow className="gap-2 items-center">
--- src/app/v2/components/forms/earn-forms/deposit-strategy-form/Summary.tsx
+++ src/app/v2/components/forms/earn-forms/deposit-strategy-form/Summary.tsx
@@ -3,7 +3,6 @@ import { useFullTokenData, DisplayValue, FlexRow, Icon, Typography, FlexCol } fr
 import { TokenDescriptionDict } from "../../../../../../shared/state/meta-data-queries/useTokenDescription";
 import { findILMStrategyByAddress, StrategyConfig } from "../../../../../state/loop-strategy/config/StrategyConfig";
 import { useFetchViewTargetMultiple } from "../../../../../state/loop-strategy/hooks/useFetchViewTargetMultiple";
-import { AssetApr } from "../../../AssetApr";
 import { StrategyApy } from "../../../AssetApy";
 import { useFormSettingsContext } from "../../contexts/useFormSettingsContext";
 import { DataRow } from "../../DataRow";
@@ -31,14 +30,11 @@ const SummaryLocal: React.FC<{
 
   const { data: strategyTokenData } = useFullTokenData(strategy.address);
 
-
   const {
     data: targetMultiple,
     isLoading: isTargetMultipleLoading,
     isFetched: isTargetMultipleFetched,
   } = useFetchViewTargetMultiple(strategy.address);
-
-
 
   return (
     <FlexCol className="rounded-card bg-neutral-100 p-6 gap-4 cursor-default">
@@ -46,19 +42,19 @@ const SummaryLocal: React.FC<{
 
       <FlexRow className="text-navy-600 justify-between">
         <Typography type="bold2">Estimated APY</Typography>
-        {asset && (
-          <StrategyApy asset={asset} className="text-navy-1000" typography="medium2" />
-        )}
-      </FlexRow>
-      <FlexRow className="text-navy-600 justify-between">
-        <Typography type="bold2">Rewards APR</Typography>
-        {asset && <AssetApr asset={asset} className="text-navy-1000" typography="medium2" />}
+        {asset && <StrategyApy asset={asset} className="text-navy-1000" typography="medium2" />}
       </FlexRow>
       <DataRow label="Action">Deposit</DataRow>
       <DataRow label="Strategy">{TokenDescriptionDict[asset]?.strategyTitle}</DataRow>
       <FlexRow className="text-navy-600 justify-between">
         <Typography type="bold2">Multiplier</Typography>
-        <DisplayValue {...targetMultiple} isLoading={isTargetMultipleLoading} isFetched={isTargetMultipleFetched} loaderSkeleton className="text-navy-1000" />
+        <DisplayValue
+          {...targetMultiple}
+          isLoading={isTargetMultipleLoading}
+          isFetched={isTargetMultipleFetched}
+          loaderSkeleton
+          className="text-navy-1000"
+        />
       </FlexRow>
       <DataRow label="Starting Asset">
         <FlexRow className="gap-2 items-center">
@@ -70,4 +66,3 @@ const SummaryLocal: React.FC<{
     </FlexCol>
   );
 };
-
