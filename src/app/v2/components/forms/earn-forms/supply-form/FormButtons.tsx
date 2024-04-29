27e0498ddf7ff6d65c2e769ef8c0d51001792ad2
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2024-04-29 12:09:40 +0200

    fix: approve decimals (#338)

--- src/app/v2/components/forms/earn-forms/supply-form/FormButtons.tsx
+++ src/app/v2/components/forms/earn-forms/supply-form/FormButtons.tsx
@@ -1,55 +1,53 @@
 import { lendingPoolAddress } from "@generated";
-import {
-  useERC20Approve,
-  FlexCol,
-  AuthGuardv2,
-  Buttonv2,
-  getApproveState,
-} from "@shared";
+import { useERC20Approve, FlexCol, AuthGuardv2, Buttonv2, getApproveState, useToken } from "@shared";
 import { useFormContext } from "react-hook-form";
-import { parseUnits, etherUnits } from "viem";
+import { parseUnits } from "viem";
 import { useFormSettingsContext } from "../../contexts/useFormSettingsContext";
 
 export const FormButtons = () => {
   const { asset, onTransaction } = useFormSettingsContext();
 
-  const { watch, formState: {
-    isSubmitting
-  } } = useFormContext();
+  const { data: tokenData } = useToken(asset);
+
+  const {
+    watch,
+    formState: { isSubmitting },
+  } = useFormContext();
   const amount = watch("amount");
 
   const { isApproved, isApproving, justApproved, approveAsync } = useERC20Approve(
     asset,
     lendingPoolAddress,
-    parseUnits(amount || "0", etherUnits.wei)
+    parseUnits(amount || "0", tokenData.decimals)
   );
 
   if (!amount) {
-    return (<Buttonv2 className="text-bold3" disabled>
-      Enter amount
-    </Buttonv2>);
+    return (
+      <Buttonv2 className="text-bold3" disabled>
+        Enter amount
+      </Buttonv2>
+    );
   }
-
 
   return (
     <FlexCol className="gap-2">
       <AuthGuardv2 message="">
-        <Buttonv2 className="text-bold3" disabled={isApproved} loading={isApproving} onClick={async () => {
-          try {
-            await approveAsync();
-          } finally {
-            onTransaction?.();
-          }
-        }}>
+        <Buttonv2
+          className="text-bold3"
+          disabled={isApproved}
+          loading={isApproving}
+          onClick={async () => {
+            try {
+              await approveAsync();
+            } finally {
+              onTransaction?.();
+            }
+          }}
+        >
           {getApproveState(isApproved, justApproved)}
         </Buttonv2>
       </AuthGuardv2>
-      <Buttonv2
-        className="text-bold3"
-        type="submit"
-        disabled={!isApproved || isSubmitting}
-        loading={isSubmitting}
-      >
+      <Buttonv2 className="text-bold3" type="submit" disabled={!isApproved || isSubmitting} loading={isSubmitting}>
         Submit
       </Buttonv2>
     </FlexCol>
