af35ffe76ac0da1c835c5a1f569b063aba41b675
Rakic <bans.cowboy@gmail.com>
2024-05-24 19:15:16 +0200

    fixed selected color

--- src/shared/components/form/inputs/InputSliderField/InputSliderField.tsx
+++ src/shared/components/form/inputs/InputSliderField/InputSliderField.tsx
@@ -26,8 +26,8 @@ const renderMarkers = (
   const numberOfSteps = max - min;
   for (let i = min; i <= max; i++) {
     let leftPercentage = ((i - min) / numberOfSteps) * 100;
-    leftPercentage += i === 0 ? 0.1 : 0;
-    const adjustment = `calc(${leftPercentage}% + (${leftPercentage} * -13px / 100))`;
+    leftPercentage -= i === 0 ? 0.1 : 0;
+    const adjustment = `calc(${leftPercentage}% + (${leftPercentage} * -12px / 100))`;
 
     markers.push(
       <button
@@ -36,10 +36,9 @@ const renderMarkers = (
         style={{
           left: adjustment,
         }}
-        className={`bottom-3.5 w-3 h-3 absolute rounded-full border-2 
-                    ${i >= enabledMin && i < enabledMax ? "border-navy-1000" : "border-gray-400 cursor-default"}
-                    ${(Number(value) || 0) > i ? "bg-navy-1000" : "bg-neutral-0"}
-                    ${(Number(value) || 0) === i ? "bg-[#22457e] border-[#22457e]" : ""}`}
+        className={`bottom-3.5 w-3 h-3 absolute rounded-full border-2 }
+                    ${(Number(value) || 0) > i ? "bg-white border-[#22457e]" : "bg-neutral-0"}
+                    ${(Number(value) || 0) === i ? "hidden" : ""}`}
         onClick={() => {
           if (i >= enabledMin && i <= enabledMax) {
             onChange?.({ target: { value: i.toString() } } as any);
