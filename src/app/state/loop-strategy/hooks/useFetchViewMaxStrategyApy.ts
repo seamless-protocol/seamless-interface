db4b1887076fdac3f14d050f760ae4073a752210
Rakic <bans.cowboy@gmail.com>
2024-05-29 20:55:43 +0200

    strategy apy refactor

--- src/app/state/loop-strategy/hooks/useFetchViewMaxStrategyApy.ts
+++ src/app/state/loop-strategy/hooks/useFetchViewMaxStrategyApy.ts
@@ -3,40 +3,12 @@ import { APY_BLOCK_FRAME } from "@meta";
 import { formatFetchNumberToViewNumber } from "../../../../shared/utils/helpers";
 import { Displayable, ViewNumber } from "src/shared/types/Displayable";
 import { Address } from "viem";
-import { StrategyAsset, useFetchStrategiesAssets } from "../metadataQueries/useFetchStrategiesAssets";
-import { calculateApy } from "./useFetchViewStrategyApy";
-import { ilmAssetStrategiesMap, StrategyData } from "../config/StrategyConfig";
-import { fetchAssetPriceInBlock } from "../../common/queries/useFetchViewAssetPrice";
-import { rainbowConfig } from "../../../config/rainbow.config";
+import { useFetchStrategiesAssets } from "../metadataQueries/useFetchStrategiesAssets";
+import { ilmAssetStrategiesMap } from "../config/StrategyConfig";
 import { useQueries } from "@tanstack/react-query";
 import { mergeQueryStates } from "@shared";
 import { semiSensitiveDataQueryConfig } from "../../settings/config";
-
-async function fetchStrategyApy(
-  strategy: StrategyData,
-  latestBlockData: any,
-  prevBlockData: any,
-  strategyAssets?: StrategyAsset
-): Promise<number> {
-  const shareValueInLatestBlock = await fetchAssetPriceInBlock(
-    rainbowConfig,
-    strategy.address,
-    latestBlockData.number,
-    strategyAssets?.debt
-  );
-  const shareValueInPrevBlock = await fetchAssetPriceInBlock(
-    rainbowConfig,
-    strategy.address,
-    prevBlockData.number,
-    strategyAssets?.debt
-  );
-
-  return calculateApy(
-    shareValueInLatestBlock || 0n,
-    shareValueInPrevBlock || 0n,
-    BigInt(latestBlockData.timestamp - prevBlockData.timestamp)
-  );
-}
+import { fetchStrategyApy } from "./useFetchViewStrategyApy";
 
 export const useFetchMaxStrategyApy = (strategy?: Address) => {
   const { data: latestBlockData } = useBlock();
@@ -45,20 +17,19 @@ export const useFetchMaxStrategyApy = (strategy?: Address) => {
   });
 
   const strategiesData = strategy ? ilmAssetStrategiesMap.get(strategy) || [] : [];
-
   const { data: assetsData, ...assetsRest } = useFetchStrategiesAssets(strategiesData.map((s) => s.address));
 
   const queryResults = useQueries({
     queries: strategiesData.map((strategy) => ({
       queryKey: ["strategyApy", strategy.address],
-      queryFn: () => fetchStrategyApy(strategy, latestBlockData, prevBlockData, assetsData[strategy.address]),
+      queryFn: () => fetchStrategyApy(strategy.address, latestBlockData, prevBlockData, assetsData[strategy.address]),
       enabled: !!latestBlockData && !!prevBlockData && !!strategy.address && !!assetsData[strategy.address],
       ...semiSensitiveDataQueryConfig, // is this okay to start using?
     })),
   });
 
   const maxApy = queryResults.reduce((max, result) => {
-    if (result.status === "success" && result.data > max) {
+    if (result.status === "success" && result.data !== undefined && result.data > max) {
       return result.data;
     }
     return max;
--- src/app/state/loop-strategy/hooks/useFetchViewStrategyApy.ts
+++ src/app/state/loop-strategy/hooks/useFetchViewStrategyApy.ts
@@ -3,10 +3,14 @@ import { APY_BLOCK_FRAME, COMPOUNDING_PERIODS_APY, SECONDS_PER_YEAR } from "@met
 import { formatFetchNumberToViewNumber, formatUnitsToNumber } from "../../../../shared/utils/helpers";
 import { FetchData, FetchNumber } from "src/shared/types/Fetch";
 import { Displayable, ViewNumber } from "src/shared/types/Displayable";
-import { useFetchAssetPriceInBlock } from "../../common/queries/useFetchViewAssetPrice";
 import { Address } from "viem";
 import { useFetchStrategyAssets } from "../metadataQueries/useFetchStrategyAssets";
 import { mergeQueryStates } from "@shared";
+import { useQuery } from "@tanstack/react-query";
+import { semiSensitiveDataQueryConfig } from "../../settings/config";
+import { rainbowConfig } from "../../../config/rainbow.config";
+import { fetchAssetPriceInBlock } from "../../common/queries/useFetchViewAssetPrice";
+import { StrategyAsset } from "../metadataQueries/useFetchStrategiesAssets";
 
 export function calculateApy(endValue: bigint, startValue: bigint, timeWindow: bigint): number {
   if (startValue === 0n || endValue === 0n || timeWindow === 0n) {
@@ -22,6 +26,34 @@ export function calculateApy(endValue: bigint, startValue: bigint, timeWindow: b
   return ((1 + apr / COMPOUNDING_PERIODS_APY) ** COMPOUNDING_PERIODS_APY - 1) * 100;
 }
 
+export async function fetchStrategyApy(
+  strategy: Address,
+  latestBlockData?: any,
+  prevBlockData?: any,
+  strategyAssets?: StrategyAsset
+): Promise<number | undefined> {
+  if (latestBlockData == null || prevBlockData == null || strategyAssets == null) return undefined;
+
+  const shareValueInLatestBlock = await fetchAssetPriceInBlock(
+    rainbowConfig,
+    strategy,
+    latestBlockData?.number,
+    strategyAssets?.debt
+  );
+  const shareValueInPrevBlock = await fetchAssetPriceInBlock(
+    rainbowConfig,
+    strategy,
+    prevBlockData.number,
+    strategyAssets?.debt
+  );
+
+  return calculateApy(
+    shareValueInLatestBlock || 0n,
+    shareValueInPrevBlock || 0n,
+    BigInt(latestBlockData.timestamp - prevBlockData.timestamp)
+  );
+}
+
 export const useFetchStrategyApy = (strategy?: Address): FetchData<FetchNumber> => {
   const { data: latestBlockData, ...latestBlockRest } = useBlock();
 
@@ -34,37 +66,18 @@ export const useFetchStrategyApy = (strategy?: Address): FetchData<FetchNumber> 
   });
 
   const { data: strategyAssets, ...strategyAssetsRest } = useFetchStrategyAssets(strategy);
-  const { data: shareValueInLatestBlock, ...latestBlockShareValueRest } = useFetchAssetPriceInBlock(
-    strategy,
-    latestBlockData?.number,
-    strategyAssets?.debt
-  );
-  const { data: shareValueInPrevBlock, ...prevBlockShareValueRest } = useFetchAssetPriceInBlock(
-    strategy,
-    prevBlockData?.number,
-    strategyAssets?.debt
-  );
 
-  // todo refactor this
-  const apy =
-    latestBlockData?.timestamp && prevBlockData?.timestamp && shareValueInLatestBlock && shareValueInPrevBlock
-      ? calculateApy(
-          shareValueInLatestBlock.bigIntValue,
-          shareValueInPrevBlock.bigIntValue,
-          latestBlockData.timestamp - prevBlockData.timestamp
-        )
-      : 0;
+  const result = useQuery({
+    queryKey: ["strategyApy", strategy],
+    queryFn: () => fetchStrategyApy(strategy!, latestBlockData, prevBlockData, strategyAssets),
+    enabled: !!latestBlockData && !!prevBlockData && !!strategy && !!strategyAssets,
+    ...semiSensitiveDataQueryConfig, // is this okay to start using?
+  });
 
   return {
-    ...mergeQueryStates([
-      latestBlockRest,
-      prevBlockRest,
-      strategyAssetsRest,
-      latestBlockShareValueRest,
-      prevBlockShareValueRest,
-    ]),
+    ...mergeQueryStates([latestBlockRest, prevBlockRest, strategyAssetsRest, result]),
     data: {
-      value: apy,
+      value: result.data,
       symbol: "%",
     },
   };
