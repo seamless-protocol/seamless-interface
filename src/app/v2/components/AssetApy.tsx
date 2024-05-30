c04b061b731e7ae20b08a75b5552dfd3c2fe0006
Rakic <bans.cowboy@gmail.com>
2024-05-30 15:12:05 +0200

    final touches on apy labels

--- src/app/v2/components/AssetApy.tsx
+++ src/app/v2/components/AssetApy.tsx
@@ -4,7 +4,6 @@ import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
 import { DisplayPercentage, DisplayPercentageProps, FlexRow, Tooltip, Typography } from "@shared";
 import { useFetchViewSupplyApy } from "../../state/lending-borrowing/hooks/useFetchViewSupplyApy";
 import { useFetchViewMaxStrategyApy } from "../../state/loop-strategy/hooks/useFetchViewMaxStrategyApy";
-import { ilmAssetStrategiesMap } from "../../state/loop-strategy/config/StrategyConfig";
 
 interface AssetApyProps extends DisplayPercentageProps {
   asset: Address;
@@ -20,23 +19,21 @@ interface StrategyApyProps extends DisplayPercentageProps {
 export const StrategyApy: React.FC<StrategyApyProps> = ({ strategy, showWarning = true, ...rest }) => {
   const { data: apy, ...restStrategyApy } = useFetchViewMaxStrategyApy(strategy);
 
-  const strategiesData = strategy ? ilmAssetStrategiesMap.get(strategy) || [] : [];
-
   if (showWarning && apy.value === 0 && !restStrategyApy.isLoading && restStrategyApy.isFetched) {
     return (
-      <FlexRow className="gap-1">
-        <DisplayPercentage {...restStrategyApy} viewValue="~" symbol={apy?.symbol} {...rest} />
-        <Tooltip tooltip={<Typography type="description">Not enough data</Typography>} size="small" theme="dark">
+      <Tooltip tooltip={<Typography type="description">Not enough data</Typography>} size="small" theme="dark">
+        <FlexRow className="gap-1">
+          <DisplayPercentage {...restStrategyApy} viewValue="~" symbol={apy?.symbol} {...rest} />
           <ExclamationTriangleIcon className="cursor-pointer" width={15} />
-        </Tooltip>
-      </FlexRow>
+        </FlexRow>
+      </Tooltip>
     );
   }
 
   return (
     <DisplayPercentage
       {...restStrategyApy}
-      viewValue={`${strategiesData?.length > 1 ? "Up to " : ""}${apy?.value === 0 ? "~" : apy?.viewValue}`}
+      viewValue={`${apy?.value === 0 ? "~" : apy?.viewValue}`}
       symbol={apy?.symbol}
       {...rest}
     />
