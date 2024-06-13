0506a4f646032267cf75df01a063fab1169397a9
Srdjan Rakic <bans.cowboy@gmail.com>
2024-06-13 16:39:13 +0200

    moved useFocusOnAssetChange to shared

--- src/shared/hooks/ui-hooks/useFocusOnAssetChange.ts
+++ src/shared/hooks/ui-hooks/useFocusOnAssetChange.ts
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from "react";
 
 export function useFocusOnAssetChange(asset?: string, focusOnAssetChange?: boolean) {
   const inputRef = useRef<HTMLInputElement>(null);
