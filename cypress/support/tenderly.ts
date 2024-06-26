76b2ef1c96e4fa01dab95d5c4e62c11d29637ee1
WingsDevelopment <bans.cowboy@gmail.com>
2024-06-26 22:23:49 +0200

    init

---
+++ cypress/support/tenderly.ts
@@ -0,0 +1,109 @@
+import { utils } from "mocha";
+
+import ERC20_ABI from "../fixtures/erc20_abi.json";
+import POOL_CONFIG_ABI from "../fixtures/poolConfig.json";
+import { english, generateMnemonic, mnemonicToAccount } from "viem/accounts";
+
+const mnemonic = generateMnemonic(english);
+const WALLET = mnemonicToAccount(mnemonic);
+
+const TENDERLY_KEY = Cypress.env("TENDERLY_KEY");
+const TENDERLY_ACCOUNT = Cypress.env("TENDERLY_ACCOUNT");
+const TENDERLY_PROJECT = Cypress.env("TENDERLY_PROJECT");
+
+export const DEFAULT_TEST_ACCOUNT = {
+  address: WALLET.address.toLowerCase(),
+};
+
+const tenderly = axios.create({
+  baseURL: "https://api.tenderly.co/api/v1/",
+  headers: {
+    "X-Access-Key": TENDERLY_KEY,
+  },
+});
+
+export class TenderlyFork {
+  public _forkNetworkID: string;
+
+  public _chainID: number;
+
+  private fork_id?: string;
+
+  constructor({ forkNetworkID }: { forkNetworkID: number }) {
+    this._forkNetworkID = forkNetworkID.toString();
+    this._chainID = 3030;
+  }
+
+  private checkForkInitialized() {
+    if (!this.fork_id) throw new Error("Fork not initialized!");
+  }
+
+  async init() {
+    const response = await tenderly.post(`account/${TENDERLY_ACCOUNT}/project/${TENDERLY_PROJECT}/fork`, {
+      network_id: this._forkNetworkID,
+      chain_config: { chain_id: this._chainID },
+    });
+    this.fork_id = response.data.simulation_fork.id;
+  }
+
+  get_rpc_url() {
+    this.checkForkInitialized();
+    return `https://rpc.tenderly.co/fork/${this.fork_id}`;
+  }
+
+  async add_balance_rpc(address: string) {
+    this.checkForkInitialized();
+    return axios({
+      url: this.get_rpc_url(),
+      method: "post",
+      headers: { "content-type": "text/plain" },
+      data: JSON.stringify({
+        jsonrpc: "2.0",
+        method: "tenderly_setBalance",
+        params: [address, "0x21e19e0c9bab2400000"],
+        id: "1234",
+      }),
+    });
+  }
+
+  //   async unpauseMarket(): Promise<void> {
+  //     const _url = this.get_rpc_url();
+  //     const provider = new JsonRpcProvider(_url);
+  //     const emergencyAdmin = "0x4365F8e70CF38C6cA67DE41448508F2da8825500";
+  //     const signer = await provider.getSigner(emergencyAdmin);
+  //     // constant addresses:
+
+  //     const poolConfigurator = new Contract("0x8145eddDf43f50276641b55bd3AD95944510021E", POOL_CONFIG_ABI, signer);
+
+  //     await poolConfigurator.setPoolPause(false, { from: signer._address, gasLimit: "4000000" });
+  //   }
+
+  //   async getERC20Token(walletAddress: string, tokenAddress: string, donorAddress?: string, tokenCount?: string) {
+  //     cy.log(`walletAddress ${walletAddress}`);
+  //     cy.log(`tokenAddress ${tokenAddress}`);
+  //     cy.log(`donorAddress ${donorAddress}`);
+  //     cy.log(`tokenCount ${tokenCount}`);
+  //     let TOP_HOLDER_ADDRESS;
+  //     const _url = this.get_rpc_url();
+  //     const provider = getDefaultProvider(_url);
+  //     if (donorAddress) {
+  //       TOP_HOLDER_ADDRESS = donorAddress;
+  //     } else {
+  //       TOP_HOLDER_ADDRESS = await this.getTopHolder(tokenAddress);
+  //     }
+  //     // @ts-ignore
+  //     const topHolderSigner = await provider.getSigner(TOP_HOLDER_ADDRESS);
+  //     const token = new Contract(tokenAddress, ERC20_ABI, topHolderSigner);
+  //     await token.transfer(walletAddress, utils.parseEther(tokenCount || "10"));
+  //   }
+
+  //   async getTopHolder(token: string) {
+  //     const res = (await axios.get(`https://api.ethplorer.io/getTopTokenHolders/${token}?apiKey=freekey`)).data.holders[0]
+  //       .address;
+  //     return res;
+  //   }
+
+  async deleteFork() {
+    await tenderly.delete(`account/${TENDERLY_ACCOUNT}/project/${TENDERLY_PROJECT}/fork/${this.fork_id}`);
+  }
+}
