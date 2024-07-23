10852d1c48dd0d58cb2946da308a2dbedc3fe1c7
Srdjan Rakic <bans.cowboy@gmail.com>
2024-07-23 22:48:38 +0200

    updates

--- cypress.config.ts
+++ cypress.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
   },
 
   e2e: {
+    // todo: read this dynamically?
     baseUrl: "http://localhost:5173",
     setupNodeEvents(on, config) {
       // implement node event listeners here
