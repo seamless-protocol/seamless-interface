ecdbf19b8551b5c5388d03de79a257e068105e6c
WingsDevelopment <bans.cowboy@gmail.com>
2024-07-30 13:42:02 +0200

    changed test mode

--- src/app/contexts/CustomWagmiProvider.tsx
+++ src/app/contexts/CustomWagmiProvider.tsx
@@ -1,36 +1,37 @@
-import React, { useEffect } from 'react'
-import { useConnect, WagmiProvider } from 'wagmi'
-import { config } from '../config/rainbow.config'
-import { testWagmiConfig } from '../config/demoConnector/testWagmiConfig'
-import { IS_DEV_MODE } from '../../globals';
-import { testConnector } from '../config/demoConnector/testConnector';
+import React, { useEffect } from "react";
+import { useConnect, WagmiProvider } from "wagmi";
+import { config } from "../config/rainbow.config";
+import { testWagmiConfig } from "../config/demoConnector/testWagmiConfig";
+import { IS_DEV_MODE } from "../../globals";
+import { testConnector } from "../config/demoConnector/testConnector";
 
 // todo: refactor this
-export const IS_IN_TEST_MODE = false;
+export const IS_IN_TEST_MODE = true;
 
 export const CustomWagmiProvider: React.FC<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }> = ({ children }) => {
-  return (IS_IN_TEST_MODE && IS_DEV_MODE) ? <TestWagmiProvider>{children}</TestWagmiProvider>
-    : <StandardWagmiProvider>{children}</StandardWagmiProvider>;
-}
+  return IS_IN_TEST_MODE && IS_DEV_MODE ? (
+    <TestWagmiProvider>{children}</TestWagmiProvider>
+  ) : (
+    <StandardWagmiProvider>{children}</StandardWagmiProvider>
+  );
+};
 
 const TestWagmiProvider: React.FC<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }> = ({ children }) => {
   // eslint-disable-next-line no-console
-  console.warn('----------------USING TEST WAGMI PROVIDER----------------');
+  console.warn("----------------USING TEST WAGMI PROVIDER----------------");
 
   return (
     <WagmiProvider config={testWagmiConfig}>
-      <TestWagmiAutoConnector>
-        {children}
-      </TestWagmiAutoConnector>
+      <TestWagmiAutoConnector>{children}</TestWagmiAutoConnector>
     </WagmiProvider>
-  )
-}
+  );
+};
 const TestWagmiAutoConnector: React.FC<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }> = ({ children }) => {
   // todo: move this auto connect into tests?
   const { connect } = useConnect();
@@ -40,14 +41,10 @@ const TestWagmiAutoConnector: React.FC<{
   }, [connect]);
 
   return children;
-}
+};
 
 const StandardWagmiProvider: React.FC<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }> = ({ children }) => {
-  return (
-    <WagmiProvider config={config}>
-      {children}
-    </WagmiProvider>
-  )
-}
+  return <WagmiProvider config={config}>{children}</WagmiProvider>;
+};
