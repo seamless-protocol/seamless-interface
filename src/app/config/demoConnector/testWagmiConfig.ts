472fdd352de3eab662d0dfd592837b05dca1fdb6
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-01 20:13:12 +0200

    fixed wagmi config

--- src/app/config/demoConnector/testWagmiConfig.ts
+++ src/app/config/demoConnector/testWagmiConfig.ts
@@ -1,4 +1,4 @@
-import { fallback, http } from "viem";
+import { fallback, http, FallbackTransport, HttpTransport } from "viem";
 import { base } from "viem/chains";
 import { createConfig } from "wagmi";
 import { createTestConnector } from "./testConnector";
@@ -6,11 +6,13 @@ import { createTestConnector } from "./testConnector";
 export const initTestWagmiConfig = (rpcUrl: string) => {
   const testConnector = createTestConnector(rpcUrl);
 
+  const fallbackTransport: FallbackTransport<[HttpTransport]> = fallback([http(rpcUrl)]);
+
   return createConfig({
     connectors: [testConnector],
     chains: [base],
     transports: {
-      [base.id]: fallback([http(rpcUrl)]),
+      [base.id]: fallbackTransport,
     },
   });
 };
