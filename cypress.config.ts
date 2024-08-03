25809a42949d36060c163937825ba47b24f9d823
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-03 15:01:51 +0200

    fixed env variables

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,12 @@ import viteConfig from "./vite.config";
 import vitePreprocessor from "cypress-vite";
 
 export default defineConfig({
+  env: {
+    url: "http://localhost:5173",
+    test_rpc: "https://virtual.base.rpc.tenderly.co/ee8497a0-46bc-4a54-8412-11aa72e813c6",
+    private_key: "0xe30165a9c8c2a7f249f1cf04ba5f2ed8afacb762cc813a66b86c898cc806d58a",
+    access_key: "RDHizgXiBFXLHy2ZhVMSZeeo26Wufpi-",
+  },
   component: {
     devServer: {
       framework: "react",
