1801eb5ef433e23b0ccf4e692b45adc64ee70d8e
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-06 18:47:47 +0200

    fixed cypress env file

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,19 @@
 import { defineConfig } from "cypress";
 import viteConfig from "./vite.config";
 import vitePreprocessor from "cypress-vite";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./.env.development" });
 
 export default defineConfig({
   env: {
     url: "http://localhost:5173",
     private_key: "0xe30165a9c8c2a7f249f1cf04ba5f2ed8afacb762cc813a66b86c898cc806d58a",
-    tenderly_test_rpc: "https://virtual.base.rpc.tenderly.co/9e91d861-0bbc-4459-bade-777df8e83da6",
-    tenderly_access_key: "RDHizgXiBFXLHy2ZhVMSZeeo26Wufpi-",
-    tenderly_account: "seamless",
-    tenderly_project: "dev",
-    test_env: "tenderly",
+    tenderly_test_rpc: process.env.VITE_TEST_RPC_URL,
+    tenderly_access_key: process.env.VITE_TEST_ACCESS_KEY,
+    tenderly_account: process.env.VITE_TEST_PROFILE,
+    tenderly_project: process.env.VITE_TEST_PROJECT,
+    test_env: process.env.VITE_TEST_ENV,
   },
   component: {
     devServer: {
