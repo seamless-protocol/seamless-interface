32d65d65d45569d0adb7de616b09c8d54e12a6b7
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-09 18:11:53 +0200

    changed env variable namings

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,11 +9,11 @@ export default defineConfig({
   env: {
     url: "http://localhost:5173",
     private_key: "0xe30165a9c8c2a7f249f1cf04ba5f2ed8afacb762cc813a66b86c898cc806d58a",
-    tenderly_test_rpc: process.env.VITE_TEST_RPC_URL,
-    tenderly_access_key: process.env.VITE_TEST_ACCESS_KEY,
-    tenderly_account: process.env.VITE_TEST_PROFILE,
-    tenderly_project: process.env.VITE_TEST_PROJECT,
-    test_env: process.env.VITE_TEST_ENV,
+    tenderly_test_rpc: process.env.VITE_CYPRESS_TEST_TENDERLY_RPC_URL,
+    tenderly_access_key: process.env.VITE_CYPRESS_TEST_TENDERLY_ACCESS_KEY,
+    tenderly_account: process.env.VITE_CYPRESS_TEST_TENDERLY_PROFILE,
+    tenderly_project: process.env.VITE_CYPRESS_TEST_TENDERLY_PROJECT,
+    test_env: process.env.VITE_CYPRESS_TEST_ENV,
   },
   component: {
     devServer: {
