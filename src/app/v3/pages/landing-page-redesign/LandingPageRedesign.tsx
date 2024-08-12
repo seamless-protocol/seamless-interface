a0b369e99e04db1f5b4ce6e0c1307854ccc929df
Srdjan Rakic <bans.cowboy@gmail.com>
2024-08-12 17:31:09 +0200

    renamed tab name

--- src/app/v3/pages/landing-page-redesign/LandingPageRedesign.tsx
+++ src/app/v3/pages/landing-page-redesign/LandingPageRedesign.tsx
@@ -2,7 +2,7 @@ import { FlexCol, FlexRow, PageContainer, TabButton, TabContent, TabProvider } f
 import { ILMsTab } from "./tabs/ilms-tab/ILMsTab";
 import { Dashboard } from "./tabs/dashboard-tab/Dashboard";
 
-type Tabs = "ILMs" | "My Positions";
+type Tabs = "ILMs" | "Dashboard";
 
 export const LandingPageRedesign = () => {
   return (
@@ -12,7 +12,7 @@ export const LandingPageRedesign = () => {
         <FlexCol className="gap-1 w-full md:max-w-[1020px]">
           <FlexRow className="w-full gap-1 border-b-navy-100 border-b-thin">
             <TabButton<Tabs> tab="ILMs">ILMs</TabButton>
-            <TabButton<Tabs> tab="My Positions">Dashboard</TabButton>
+            <TabButton<Tabs> tab="Dashboard">Dashboard</TabButton>
           </FlexRow>
 
           <TabContent<Tabs> tab="ILMs">
@@ -20,7 +20,7 @@ export const LandingPageRedesign = () => {
               <ILMsTab />
             </div>
           </TabContent>
-          <TabContent<Tabs> tab="My Positions">
+          <TabContent<Tabs> tab="Dashboard">
             <div className="mt-8">
               <Dashboard />
             </div>
