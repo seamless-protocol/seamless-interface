5abfb03185494504cd737addd14951de43fea7df
kitanovicd <kitanovic.danilo@outlook.com>
2024-08-21 14:09:34 +0200

    fix: test

--- src/app/statev3/hooks/user-strategy-profit/UserStrategyProfit.math.test.ts
+++ src/app/statev3/hooks/user-strategy-profit/UserStrategyProfit.math.test.ts
@@ -1,17 +1,17 @@
 import { test, expect } from "vitest";
 import { Address, formatUnits, parseUnits, zeroAddress } from "viem";
-import { LogWithStrategyPrice, cUserStrategyProfit } from "./UserStrategyProfit.math";
+import { TransfersWithStrategyPrice, cUserStrategyProfit } from "./UserStrategyProfit.math";
 
 const USER: Address = "0x0000000000000000000000000000000000000001";
 const STRATEGY_DECIMALS: number = 18;
 const DEFAULT_STRATEGY_CURRENT_PRICE: bigint = parseUnits("2000", 8);
 
 function cUserStrategyProfitWrapper(
-  logs: LogWithStrategyPrice[],
+  transfers: TransfersWithStrategyPrice[],
   currStrategyPrice: bigint = DEFAULT_STRATEGY_CURRENT_PRICE
 ) {
   const { realizedProfit, unrealizedProfit, unrealizedProfitPercentage } = cUserStrategyProfit({
-    logs,
+    transfers,
     user: USER,
     currStrategyPrice: currStrategyPrice || DEFAULT_STRATEGY_CURRENT_PRICE,
     strategyDecimals: STRATEGY_DECIMALS,
@@ -31,20 +31,15 @@ enum Action {
 
 function actionLog({ action, amount, price }: { action: Action; amount: string; price: string }) {
   return {
-    log: {
-      args: {
-        from: action === Action.BUY ? zeroAddress : USER,
-        to: action === Action.BUY ? USER : zeroAddress,
-        value: parseUnits(amount, STRATEGY_DECIMALS),
-      },
-      blockNumber: 0n,
-    },
+    from: action === Action.BUY ? zeroAddress : USER,
+    to: action === Action.BUY ? USER : zeroAddress,
+    value: parseUnits(amount, STRATEGY_DECIMALS),
     strategyPrice: parseUnits(price, 8),
   };
 }
 
 test("cUserStrategyProfit", async () => {
-  const logs: LogWithStrategyPrice[] = [];
+  const logs: TransfersWithStrategyPrice[] = [];
 
   let result = cUserStrategyProfitWrapper(logs);
   expect(result.realizedProfit).toBe(0);
