71623c6cad39ed144980ff0b54484967d84678c1
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-27 09:10:51 +0200

    fixed mobile audit component

--- src/app/v3/components/banner/Audited.tsx
+++ src/app/v3/components/banner/Audited.tsx
@@ -11,10 +11,10 @@ export const Audited = () => {
   const { data, ...rest } = useFetchViewLendingPoolInfo();
 
   return (
-    <div className="flex justify-center">
+    <div className="flex justify-center px-2 md:px-0">
       <div className="w-full max-w-page-content">
         <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-blueGradient rounded-[100px] text-white">
-          <FlexRow className="items-center justify-between w-full">
+          <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-between w-full">
             <FlexRow className="items-center gap-2">
               <Typography type="medium3">Audited by</Typography>
               <Icon className="mt-[4px]" src={certoraIcon} alt="certora-logo" width={82} height={24} />
