b96c8776482fd24925a7cb23b60a68346f9884b8
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-27 16:50:35 +0200

    renamed prop

--- src/app/v3/utils/uiUtils.ts
+++ src/app/v3/utils/uiUtils.ts
@@ -1,12 +1,12 @@
 import polygonPositiveSvg from "@assets/common/polygon-positive.svg";
 import polygonNegativeSvg from "@assets/common/polygon-negative.svg";
 
-export const getColorBasedOnSign = (apy: number | string | undefined): string | undefined => {
-  if (!apy) return undefined;
-  return Number(apy || 0) >= 0 ? "text-success-900" : "text-error-1000";
+export const getColorBasedOnSign = (value: number | string | undefined): string | undefined => {
+  if (!value) return undefined;
+  return Number(value || 0) >= 0 ? "text-success-900" : "text-error-1000";
 };
 
-export const getSvgBasedOnSign = (apy: number | string | undefined): string | undefined => {
-  if (!apy) return undefined;
-  return Number(apy || 0) >= 0 ? polygonPositiveSvg : polygonNegativeSvg;
+export const getSvgBasedOnSign = (value: number | string | undefined): string | undefined => {
+  if (!value) return undefined;
+  return Number(value || 0) >= 0 ? polygonPositiveSvg : polygonNegativeSvg;
 };
