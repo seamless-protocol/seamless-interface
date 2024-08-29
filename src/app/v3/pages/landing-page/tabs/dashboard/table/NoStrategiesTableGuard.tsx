4643a0659a29fc5b1854ae8e09c998791570356f
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-29 17:21:07 +0200

    fixed mobile versions

--- src/app/v3/pages/landing-page/tabs/dashboard/table/NoStrategiesTableGuard.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/table/NoStrategiesTableGuard.tsx
@@ -12,25 +12,26 @@ export const NoStrategiesTableGuard: React.FC<{
     return <>{children}</>;
   }
 
+  if (!rest.isFetched || rest.isLoading)
+    return (
+      <FlexCol className="justify-center items-center">
+        <div className="loading loading-spinner flex self-center min-h-40" />
+      </FlexCol>
+    );
+
   return (
     <FlexCol className=" justify-center items-center gap-8 py-14">
-      {rest.isFetched || !rest.isLoading ? (
-        <>
-          <Image src={notConnectedImage} alt="no-strategies" width={160} height={160} />
-          <FlexCol className="gap-2 justify-center items-center">
-            <Typography type="bold5">You have no positions.</Typography>
-            <Typography type="medium3">
-              Go back to{" "}
-              <Link className="underline" to={RouterConfig.Routes.landingPage}>
-                ILM tab
-              </Link>{" "}
-              to create a new position!
-            </Typography>
-          </FlexCol>
-        </>
-      ) : (
-        <div className="loading loading-spinner flex self-center min-h-40" />
-      )}
+      <Image src={notConnectedImage} alt="no-strategies" width={160} height={160} />
+      <FlexCol className="gap-2 justify-center items-center">
+        <Typography type="bold5">You have no positions.</Typography>
+        <Typography type="medium3">
+          Go back to{" "}
+          <Link className="underline" to={RouterConfig.Routes.landingPage}>
+            ILM tab
+          </Link>{" "}
+          to create a new position!
+        </Typography>
+      </FlexCol>
     </FlexCol>
   );
 };
--- src/app/v3/pages/landing-page/tabs/ilms/table/LoadingTableGuard.tsx
+++ src/app/v3/pages/landing-page/tabs/ilms/table/LoadingTableGuard.tsx
@@ -6,13 +6,11 @@ export const LoadingTableGuard: React.FC<{
 }> = ({ loadingState: numberOfStrategiesDisplayable, children }) => {
   const { ...rest } = numberOfStrategiesDisplayable;
 
+  if (rest.isFetched || !rest.isLoading) return <>{children}</>;
+
   return (
-    <FlexCol className=" justify-center items-center gap-8 py-14">
-      {rest.isFetched || !rest.isLoading ? (
-        <>{children}</>
-      ) : (
-        <div className="loading loading-spinner flex self-center min-h-40" />
-      )}
+    <FlexCol className="justify-center items-center">
+      <div className="loading loading-spinner flex self-center min-h-40" />
     </FlexCol>
   );
 };
