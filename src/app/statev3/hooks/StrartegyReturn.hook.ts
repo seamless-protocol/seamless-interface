b0254c792a7ef17a4d0c4cf8d4aa46954f3b0440
kitanovicd <kitanovic.danilo@outlook.com>
2024-08-30 16:21:00 +0200

    feat: 30d return

--- src/app/statev3/hooks/StrartegyReturn.hook.ts
+++ src/app/statev3/hooks/StrartegyReturn.hook.ts
@@ -4,7 +4,15 @@ import { getConfig } from "../../utils/queryContractUtils";
 import { fetchAssetPriceInBlock } from "../queries/AssetPrice.hook";
 import { APY_BLOCK_FRAME, PERCENTAGE_VALUE_DECIMALS, USD_VALUE_DECIMALS } from "../../../meta";
 import { fetchStrategyAssets } from "../metadata/StrategyAssets.fetch";
-import { FetchBigIntStrict, formatFetchBigInt } from "../../../shared";
+import {
+  Displayable,
+  FetchBigIntStrict,
+  ViewBigInt,
+  formatFetchBigInt,
+  formatFetchBigIntToViewBigInt,
+} from "../../../shared";
+import { useQuery } from "@tanstack/react-query";
+import { disableCacheQueryConfig } from "../../state/settings/queryConfig";
 
 interface cStrategyReturnInput {
   currStrategyPrice: bigint;
@@ -20,13 +28,12 @@ export function cStrategyReturn(input: cStrategyReturnInput): FetchBigIntStrict 
   const prevStrategyPriceInDebAsset = (prevStrategyPrice * parseUnits("1", USD_VALUE_DECIMALS)) / prevDebtAssetPrice;
 
   const strategyReturn =
-    ((currStrategyPriceInDebAsset - prevStrategyPriceInDebAsset) * parseUnits("1", PERCENTAGE_VALUE_DECIMALS)) /
-    prevStrategyPriceInDebAsset;
+    ((currStrategyPriceInDebAsset - prevStrategyPriceInDebAsset) * 10000n) / prevStrategyPriceInDebAsset;
 
   return formatFetchBigInt(strategyReturn, PERCENTAGE_VALUE_DECIMALS, "%");
 }
 
-export async function fetchStrategyHistoricReturn(strategy: Address) {
+export async function fetchStrategyHistoricReturn(strategy: Address): Promise<FetchBigIntStrict> {
   const [{ debt: debtAsset }, latestBlock] = await Promise.all([fetchStrategyAssets(strategy), getBlock(getConfig())]);
 
   const [currStrategyPrice, prevStrategyPrice, currDebtAssetPrice, prevDebtAssetPrice] = await Promise.all([
@@ -43,3 +50,17 @@ export async function fetchStrategyHistoricReturn(strategy: Address) {
     prevDebtAssetPrice: prevDebtAssetPrice.bigIntValue,
   });
 }
+
+export const useFetchFormattedStrategyHistoricReturn = (strategy?: Address): Displayable<ViewBigInt> => {
+  const { data, ...rest } = useQuery({
+    queryKey: ["hookFormattedStrategyHistoricReturn", strategy],
+    queryFn: () => fetchStrategyHistoricReturn(strategy!),
+    enabled: !!strategy,
+    ...disableCacheQueryConfig,
+  });
+
+  return {
+    ...rest,
+    data: formatFetchBigIntToViewBigInt(data),
+  };
+};
