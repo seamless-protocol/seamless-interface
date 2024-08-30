a79db3a661c02c33b0852fd40b62c29bb7d62ac9
WingsDevelopment <bans.cowboy@gmail.com>
2024-08-30 23:15:16 +0200

    added apy for strategy in summary

--- src/app/v3/components/forms/withdraw-form/Summary.tsx
+++ src/app/v3/components/forms/withdraw-form/Summary.tsx
@@ -4,6 +4,7 @@ import { useAccount } from "wagmi";
 import { useFetchViewWithdrawCostInUsdAndUnderlying } from "../../../../state/loop-strategy/hooks/useFetchWithdrawCostInUsdAndUnderlying";
 import { checkAuthentication } from "../../../../utils/authenticationUtils";
 import { useFormSettingsContext } from "../contexts/useFormSettingsContext";
+import { AssetApy } from "../../../../v2/components/asset-data/AssetApy";
 
 export const Summary: React.FC<{
   debouncedAmount: string;
@@ -17,9 +18,14 @@ export const Summary: React.FC<{
       <FlexCol className="rounded-card bg-background-selected p-6 gap-4">
         <Typography type="bold3">Summary</Typography>
 
+        <FlexRow className="text-navy-600 justify-between">
+          <Typography type="bold2">Estimated Total APY</Typography>
+          {strategy && <AssetApy asset={strategy} subStrategy={strategy} isStrategy className="text-navy-1000" />}
+        </FlexRow>
+
         <FlexRow className="justify-between text-navy-600">
           <FlexRow className="gap-1 items-center">
-            <Typography type="bold2">Max Transaction cost</Typography>
+            <Typography type="bold2">3rd party DEX fees</Typography>
             <StandardTooltip width={1}>
               <Typography type="medium2" className="text-navy-1000">
                 DEX fees and price impact incurred to keep the strategy <br /> at the target multiple after your
