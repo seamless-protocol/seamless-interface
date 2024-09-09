8d8783c0da413fb26be91436a3519b20a6a73226
WingsDevelopment <bans.cowboy@gmail.com>
2024-09-09 17:08:11 +0200

    fix claim bug (#581)

--- src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/claim-button/ClaimModal.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/claim-button/ClaimModal.tsx
@@ -7,13 +7,10 @@ import {
   DisplayMoney,
   ViewBigInt,
   useNotificationContext,
-  MyFormProvider,
   ModalHandles,
 } from "@shared";
-import { useForm } from "react-hook-form";
 import React, { useRef } from "react";
 import { useMutateClaimAllRewards } from "../../../../../../../state/loop-strategy/mutations/useMutateClaimAllRewards";
-import { FormSettingsProvider } from "../../../../../../../v2/components/forms/contexts/FormSettingsContext";
 import TokenRow from "./TokenRow";
 
 interface Reward {
@@ -33,10 +30,6 @@ export const ClaimModal: React.FC<ClaimModalProps> = ({ totalRewards, rewards, d
   const { claimAllAsync, isPending } = useMutateClaimAllRewards();
 
   const { showNotification } = useNotificationContext();
-
-  const methods = useForm({});
-
-  const { handleSubmit } = methods;
 
   const onSubmitAsync = async () => {
     await claimAllAsync({
@@ -58,46 +51,42 @@ export const ClaimModal: React.FC<ClaimModalProps> = ({ totalRewards, rewards, d
   };
 
   return (
-    <FormSettingsProvider>
-      <MyFormProvider methods={methods} onSubmit={handleSubmit(onSubmitAsync)}>
-        <Modal
-          ref={modalRef}
-          header="Claim rewards"
-          size="small"
-          // TODO: Remove this when generic button component is ready
-          buttonProps={{
-            children: "Claim",
-            className: `text-bold3 rounded-button text-neutral-0 py-3 px-4 ${disabled ? "bg-primary-300" : "bg-metalic"}`,
-            disabled,
-          }}
-        >
-          <FlexCol className="gap-8">
-            <FlexCol className="gap-6">
-              <FlexCol className="gap-1">
-                <Typography type="bold2">Transaction overview</Typography>
-                <FlexCol className="gap-5 shadow-card rounded-card bg-background-selected p-4 pt-6">
-                  {rewards?.map((token, index) => (
-                    <TokenRow
-                      key={index}
-                      logo={token.logo}
-                      tokenAmount={token.tokenAmount}
-                      dollarAmount={token.dollarAmount}
-                    />
-                  ))}
-                  <FlexRow className="justify-between items-center mt-4">
-                    <Typography type="bold1">Total worth</Typography>
-                    <DisplayMoney typography="bold2" {...totalRewards} symbolPosition="before" />
-                  </FlexRow>
-                </FlexCol>
-              </FlexCol>
-
-              <Buttonv2 className="text-bold2" type="submit" loading={isPending}>
-                Claim all rewards
-              </Buttonv2>
+    <Modal
+      ref={modalRef}
+      header="Claim rewards"
+      size="small"
+      // TODO: Remove this when generic button component is ready
+      buttonProps={{
+        children: "Claim",
+        className: `text-bold3 rounded-button text-neutral-0 py-3 px-4 ${disabled ? "bg-primary-300" : "bg-metalic"}`,
+        disabled,
+      }}
+    >
+      <FlexCol className="gap-8">
+        <FlexCol className="gap-6">
+          <FlexCol className="gap-1">
+            <Typography type="bold2">Transaction overview</Typography>
+            <FlexCol className="gap-5 shadow-card rounded-card bg-neutral-100 p-4 pt-6">
+              {rewards?.map((token, index) => (
+                <TokenRow
+                  key={index}
+                  logo={token.logo}
+                  tokenAmount={token.tokenAmount}
+                  dollarAmount={token.dollarAmount}
+                />
+              ))}
+              <FlexRow className="justify-between items-center mt-4">
+                <Typography type="bold1">Total worth</Typography>
+                <DisplayMoney typography="bold2" {...totalRewards} symbolPosition="before" />
+              </FlexRow>
             </FlexCol>
           </FlexCol>
-        </Modal>
-      </MyFormProvider>
-    </FormSettingsProvider>
+
+          <Buttonv2 className="text-bold2" onClick={onSubmitAsync} loading={isPending}>
+            Claim all rewards
+          </Buttonv2>
+        </FlexCol>
+      </FlexCol>
+    </Modal>
   );
 };
