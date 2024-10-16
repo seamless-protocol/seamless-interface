fdc346510293bbaeb9ff6771223bf9a4fc270071
WingsDevelopment <bans.cowboy@gmail.com>
2024-10-16 20:10:36 +0200

    feat: Update banner (#612)

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -6,11 +6,11 @@ export const SurveyBanner = () => {
   return (
     <div className="flex justify-center px-2 md:px-0">
       <div className="w-full max-w-page-content">
-        <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-orange-400 rounded-[100px] text-black">
+        <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-lime-500 rounded-[100px] text-black">
           <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-between w-full">
             <Typography type="bold3">
-              📣 <strong>NEW SURVEY ! </strong> Share Your thoughts! Help Seamless enhance ILMs by taking{" "}
-              <strong>this new and</strong> quick 5-minute survey. 👉🏿{" "}
+              📣 Are you a Base DeFi power user? Seamless is developing a new product and wants to learn about your
+              needs 👉🏿{" "}
               <Link to={seamlessSurveyUrl} className="underline" target="_blank" rel="noreferrer noopen">
                 Take the Survey Here
               </Link>
