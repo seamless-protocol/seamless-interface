aeeb2c3416aef7e5cb717e3da6f6c7abcaa93964
WingsDevelopment <bans.cowboy@gmail.com>
2024-10-29 23:19:24 +0100

    centered banner text (#614)

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -5,7 +5,7 @@ export const SurveyBanner = () => {
     <div className="flex justify-center px-2 md:px-0">
       <div className="w-full max-w-page-content">
         <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-lime-500 rounded-[100px] text-black">
-          <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-between w-full">
+          <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-center w-full">
             <Typography type="bold3">📣 Stay tuned, Community Blog Post coming soon! 📣</Typography>
           </FlexRow>
         </div>
