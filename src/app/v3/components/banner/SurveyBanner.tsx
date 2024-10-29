1379689fd56ba5608b3444756ec5c31439412068
WingsDevelopment <bans.cowboy@gmail.com>
2024-10-29 20:37:38 +0100

    banner updated (#613)

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -1,6 +1,4 @@
 import { FlexRow, Typography } from "@shared";
-import { Link } from "react-router-dom";
-import { seamlessSurveyUrl } from "@router";
 
 export const SurveyBanner = () => {
   return (
@@ -8,13 +6,7 @@ export const SurveyBanner = () => {
       <div className="w-full max-w-page-content">
         <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-lime-500 rounded-[100px] text-black">
           <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-between w-full">
-            <Typography type="bold3">
-              📣 Are you a Base DeFi power user? Seamless is developing a new product and wants to learn about your
-              needs 👉🏿{" "}
-              <Link to={seamlessSurveyUrl} className="underline" target="_blank" rel="noreferrer noopen">
-                Take the Survey Here
-              </Link>
-            </Typography>
+            <Typography type="bold3">📣 Stay tuned, Community Blog Post coming soon! 📣</Typography>
           </FlexRow>
         </div>
       </div>
