fe30358a1130e60e9490e3e54832606fa3fd2cf4
WingsDevelopment <bans.cowboy@gmail.com>
2024-11-21 20:47:41 +0100

    feat: Banner update (#626)

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -1,12 +1,16 @@
-import { FlexRow, Typography } from "@shared";
+import { FlexRow } from "@shared";
+import { disourseUrl } from "@router";
+import { Link } from "react-router-dom";
 
 export const SurveyBanner = () => {
   return (
     <div className="flex justify-center px-2 md:px-0">
       <div className="w-full max-w-page-content">
-        <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-lime-500 rounded-[100px] text-black">
-          <FlexRow className="md:flex-row flex-col md:gap-0 gap-4 items-center justify-center w-full">
-            <Typography type="bold3">📣 Stay tuned, Community Blog Post coming soon! 📣</Typography>
+        <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-[#5CDBF0] rounded-[100px] text-black">
+          <FlexRow className="md:gap-1 gap-4 items-center justify-center w-full">
+            <p className="text-bold3">📣 Discover Seamless&apos;s vision and plans for the future
+              <Link className="text-bold3 underline" target="_blank" rel="noreferrer" to={disourseUrl}> here</Link>
+              !📣</p>
           </FlexRow>
         </div>
       </div>
