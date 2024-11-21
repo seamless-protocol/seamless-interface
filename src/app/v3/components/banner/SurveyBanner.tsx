df4e3bd1eb629c8be2c721557a36715671b3d5ec
WingsDevelopment <bans.cowboy@gmail.com>
2024-11-21 22:18:11 +0100

    updated emojis

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -8,9 +8,13 @@ export const SurveyBanner = () => {
       <div className="w-full max-w-page-content">
         <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-[#5CDBF0] rounded-[100px] text-black">
           <FlexRow className="md:gap-1 gap-4 items-center justify-center w-full">
-            <p className="text-bold3">📣 Discover Seamless&apos;s vision and plans for the future
-              <Link className="text-bold3 underline" target="_blank" rel="noreferrer" to={disourseUrl}> here</Link>
-              !📣</p>
+            <p className="text-bold3">
+              💫 Discover Seamless&apos;s vision and plans for the future{" "}
+              <Link className="text-bold3 underline" target="_blank" rel="noreferrer" to={disourseUrl}>
+                here
+              </Link>
+              ! 💫
+            </p>
           </FlexRow>
         </div>
       </div>
