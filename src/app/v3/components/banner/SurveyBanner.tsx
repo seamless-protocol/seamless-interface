75f48497805220b6f26b89effc4a267c57095df5
Srdjan Rakic <bans.cowboy@gmail.com>
2024-11-21 19:10:18 +0100

    finished banner

--- src/app/v3/components/banner/SurveyBanner.tsx
+++ src/app/v3/components/banner/SurveyBanner.tsx
@@ -7,10 +7,10 @@ export const SurveyBanner = () => {
     <div className="flex justify-center px-2 md:px-0">
       <div className="w-full max-w-page-content">
         <div className="flex flex-col justify-center text-center w-full md:flex-row py-4 px-8 bg-[#5CDBF0] rounded-[100px] text-black">
-          <FlexRow className="md:flex-row flex-col md:gap-1 gap-4 items-center justify-center w-full">
-            <Typography type="bold3">📣 Discover Seamless&apos;s vision and plans for the future</Typography>
-            <Link className="text-bold3 underline" target="_blank" rel="noreferrer" to={disourseUrl}> here</Link>
-            <Typography type="bold3"> !📣</Typography>
+          <FlexRow className="md:gap-1 gap-4 items-center justify-center w-full">
+            <p className="text-bold3">📣 Discover Seamless&apos;s vision and plans for the future
+              <Link className="text-bold3 underline" target="_blank" rel="noreferrer" to={disourseUrl}> here</Link>
+              !📣</p>
           </FlexRow>
         </div>
       </div>
