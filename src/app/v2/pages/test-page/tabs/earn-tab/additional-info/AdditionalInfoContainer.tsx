b655fe7d546897cb9c3e261633ee39bb7d3c5eed
Srdjan Rakic <bans.cowboy@gmail.com>
2024-12-02 13:52:17 +0100

    deleting v2 folder

--- src/app/v2/pages/test-page/tabs/earn-tab/additional-info/AdditionalInfoContainer.tsx
+++
@@ -1,9 +0,0 @@
-import { AdditionalInfo } from "./AdditionalInfo";
-import { useFormSettingsContext } from "../../../../../components/forms/contexts/useFormSettingsContext";
-
-export const AdditionalInfoContainer = () => {
-  const { subStrategy, asset } = useFormSettingsContext();
-  if (!asset) return null;
-
-  return <AdditionalInfo asset={asset} subStrategy={subStrategy} />;
-};
