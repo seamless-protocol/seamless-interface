24f4bcabb442d12abfb654acd491533a45034f08
WingsDevelopment <bans.cowboy@gmail.com>
2025-01-09 12:10:11 +0100

    removed shared component

--- src/app/v3/components/graph/GraphSpinner.tsx
+++
@@ -1,5 +0,0 @@
-export const GraphSpinner = () => (
-  <div className="flex items-center justify-center h-[90%] w-full absolute inset-0 z-10">
-    <div className="loading loading-spinner" />
-  </div>
-);
