8fd0cc20e8fe6f51fe267d1ffcc72a93071db9c5
Srdjan Rakic <bans.cowboy@gmail.com>
2025-01-16 19:25:14 +0100

    modal refactor

--- src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/morpho-vaults/rewards/ClaimModal.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/morpho-vaults/rewards/ClaimModal.tsx
@@ -1,16 +1,10 @@
 import {
-  FlexCol,
-  FlexRow,
-  Modal,
-  Typography,
-  Buttonv2,
-  DisplayMoney,
   ViewBigInt,
   ModalHandles,
   Token,
 } from "@shared";
 import React, { useRef } from "react";
-import TokenRow from "./TokenRow";
+import { ClaimModalComponent } from "../../../components/common/ClaimModalComponent";
 
 interface Reward {
   claimableNow?: ViewBigInt;
@@ -26,6 +20,7 @@ interface ClaimModalProps {
 
 export const ClaimModal: React.FC<ClaimModalProps> = ({ totalUsdValue, rewards, disabled }) => {
   const modalRef = useRef<ModalHandles | null>(null);
+  const isPending = false;
 
   const onSubmitAsync = async () => {
     // eslint-disable-next-line no-console
@@ -33,43 +28,19 @@ export const ClaimModal: React.FC<ClaimModalProps> = ({ totalUsdValue, rewards, 
   };
 
   return (
-    <div>
-      <Modal
-        ref={modalRef}
-        header="Claim rewards"
-        size="small"
-        buttonProps={{
-          children: "Claim",
-          className: `text-bold3 rounded-button text-neutral-0 py-3 px-4 ${disabled ? "bg-primary-300" : "bg-metalic"}`,
-          disabled,
-        }}
-      >
-        <FlexCol className="gap-8">
-          <FlexCol className="gap-6">
-            <FlexCol className="gap-1">
-              <Typography type="bold2">Transaction overview</Typography>
-              <FlexCol className="gap-5 shadow-card rounded-card bg-neutral-100 p-4 pt-6">
-                {rewards?.map((reward, index) => (
-                  <TokenRow
-                    key={index}
-                    logo={reward.token.logo}
-                    tokenAmount={reward.claimableNowUsd}
-                    dollarAmount={reward.claimableNow}
-                  />
-                ))}
-                <FlexRow className="justify-between items-center mt-4">
-                  <Typography type="bold1">Total worth</Typography>
-                  <DisplayMoney typography="bold2" {...totalUsdValue} symbolPosition="before" />
-                </FlexRow>
-              </FlexCol>
-            </FlexCol>
-
-            <Buttonv2 className="text-bold2" onClick={onSubmitAsync} loading={false}>
-              Claim all rewards
-            </Buttonv2>
-          </FlexCol>
-        </FlexCol>
-      </Modal>
-    </div>
+    <ClaimModalComponent
+      modalRef={modalRef}
+      headerText="Claim rewards"
+      buttonText="Claim"
+      rewards={rewards?.map((reward) => ({
+        tokenAmount: reward.claimableNow,
+        dollarAmount: reward.claimableNowUsd,
+        logo: reward.token.logo,
+      }))}
+      totalRewards={totalUsdValue}
+      disabled={disabled}
+      onSubmit={onSubmitAsync}
+      isPending={isPending}
+    />
   );
 };
