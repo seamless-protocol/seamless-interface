c5759eb07d7f950d67ba660995d2cc287e50fe79
Srdjan Rakic <bans.cowboy@gmail.com>
2025-01-17 11:43:07 +0100

    updates

--- src/app/statev3/morpho/user-distributions/useMutateClaimAllMorphoRewards.ts
+++ src/app/statev3/morpho/user-distributions/useMutateClaimAllMorphoRewards.ts
@@ -1,4 +1,4 @@
-import { SeamlessWriteAsyncParams, useSeamlessSendTransaction } from "@shared";
+import { getParsedError, SeamlessWriteAsyncParams, useNotificationContext, useSeamlessSendTransaction } from "@shared";
 import { fetchMorphoUserDistributions } from "./MorphoUserDistributions.fetch";
 import { useAccount } from "wagmi";
 import { BundlerAction } from "@morpho-org/morpho-blue-bundlers/pkg";
@@ -14,10 +14,12 @@ export const useMutateClaimAllMorphoRewards = () => {
   const { address } = useAccount();
 
   const { bundler } = getMorphoChainAddresses(ChainId.BaseMainnet);
+  const { showNotification } = useNotificationContext();
 
   // hook call
   const { sendTransactionAsync, ...rest } = useSeamlessSendTransaction({
     queriesToInvalidate: [[MORPHO_USER_REWARDS_QUERY_KEY]],
+    hideDefaultErrorOnNotification: true,
   });
 
   // mutation wrapper
@@ -26,20 +28,17 @@ export const useMutateClaimAllMorphoRewards = () => {
       if (!address) throw new Error("Account address is not found. Please connect your wallet.");
 
       const distributions = await fetchMorphoUserDistributions(address);
-
-      const actions: any = [];
-      distributions.data.map((item) =>
-        actions.push(
-          BundlerAction.urdClaim(
-            item.distributor.address,
-            address,
-            item.asset.address,
-            item.claimable,
-            item.proof,
-            false // todo : doublecheck
-          )
+      const actions: BundlerAction[] = distributions.data.map((item) =>
+        BundlerAction.urdClaim(
+          item.distributor.address,
+          address,
+          item.asset.address,
+          item.claimable,
+          item.proof,
+          false
         )
       )
+      if (!actions || actions.length === 0) throw new Error("No rewards to claim");
 
       const data = encodeFunctionData({
         abi: baseBundlerAbi,
@@ -54,8 +53,10 @@ export const useMutateClaimAllMorphoRewards = () => {
         },
         { ...settings }
       );
+
     } catch (error) {
       console.error("Failed to claim all rewards", error);
+      showNotification({ status: "error", content: `Failed to claim all rewards: ${getParsedError(error)}` });
     }
   };
 
