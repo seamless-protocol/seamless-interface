ec36f80e1871375b06687dbfb5b681a6aa861b45
WingsDevelopment <bans.cowboy@gmail.com>
2025-01-26 18:36:37 +0100

    lock files updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { sentryVitePlugin } from "@sentry/vite-plugin";
-import { nodePolyfills } from "vite-plugin-node-polyfills";
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
@@ -11,7 +10,6 @@ export default defineConfig(({ mode }) => {
   return {
     base: "",
     plugins: [
-      nodePolyfills(),
       react(),
       sentryVitePlugin({
         org: "seamless-jw",
