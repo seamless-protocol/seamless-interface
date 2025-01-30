e0063d8dfcad088dc4b6b9177388b8d31b57d31c
Srdjan Rakic <bans.cowboy@gmail.com>
2025-01-30 18:00:35 +0100

    updated error case in forms

--- src/app/v3/components/forms/morpho-vault/withdraw-form/MorphoWithdrawForm.tsx
+++ src/app/v3/components/forms/morpho-vault/withdraw-form/MorphoWithdrawForm.tsx
@@ -19,13 +19,11 @@ export const MorphoWithdrawForm = () => {
   if (!vaultData || error) {
     // eslint-disable-next-line no-console
     console.warn("Vault not found!!!");
-    if (error) {
-      console.error('MorphoDepositForm error while fetching full vault info', error)
-      return <div className="min-h-[300px]" >
-        <Typography type="medium3" className="text-red-600">Error while fetching full vault info: {error?.message}</Typography>
-      </div>
-    };
-    return <div className="min-h-[300px]" />;
+    if (error) console.error('MorphoWithdrawForm error while fetching full vault info', error);
+
+    return <div className="min-h-[300px]" >
+      <Typography type="medium3" className="text-red-600">Error while fetching full vault info: {error?.message}</Typography>
+    </div>
   }
 
   return <MoprhoVaultFormLocal vaultData={vaultData} />;
