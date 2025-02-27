2ad9a65ab040a531bdb8ecb1beb6012cdf6488a0
jcislo <pleja918@gmail.com>
2025-02-26 23:02:11 -0500

    add claim rewards window and cleanup main staking component

--- src/app/v3/pages/landing-page/tabs/staking/components/StakingPage.tsx
+++ src/app/v3/pages/landing-page/tabs/staking/components/StakingPage.tsx
@@ -1,5 +1,6 @@
 import { FlexCol, FlexRow, PageContainer } from "@shared";
 import { useNavigate, useParams } from "react-router-dom";
+import { UnclaimedRewardsBox } from "./UnclaimedRewardsBox";
 
 import { Address } from "viem";
 
@@ -15,24 +16,13 @@ export const StakingPage = () => {
   return (
     <PageContainer className="flex justify-center py-6 pb-12 px-4 md:px-0">
       <FlexCol className="gap-1 w-full md:max-w-page-content">
-        
-
-        <div>
-            <div className="mb-8">{/* <StrategyHeading /> */}</div>
-
-            <div className="grid grid-cols-1 md:grid-cols-[2fr,1fr] gap-8 w-full items-start border border">
-              <div className="md:sticky top-6 order-1 md:order-2 md:min-w-[460px]">
-                  <FormContainer />
-              </div>
-
-              <div className="flex flex-col gap-10 order-2 md:order-1">
-                  {/* {isConnected && <CurrentHoldings address={vault} />}
-                  <TotalAssetsGraphComponent />
-                  <NetApyGraphComponent vault={vault} />
-                  <MorphoVaultStats vault={vault} />
-                  <VaultDetails /> */}
-              </div>
-            </div>
+        <div className="grid grid-rows-1 md:grid-cols-[2fr,1fr] gap-8 w-full items-start border border">
+          <div className="flex-grow">
+            <UnclaimedRewardsBox />
+          </div>
+          <div className="md:sticky top-6 order-1 md:order-2 md:min-w-[460px]">
+              <FormContainer />
+          </div>
         </div>
       </FlexCol>
     </PageContainer>
