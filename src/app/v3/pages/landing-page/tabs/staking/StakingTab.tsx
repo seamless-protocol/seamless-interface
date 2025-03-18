8042a4d3c10c8ef14e413cf87f2b12c34ad45618
WingsDevelopment <bans.cowboy@gmail.com>
2025-03-18 13:16:49 +0100

    additional updated based on comments

--- src/app/v3/pages/landing-page/tabs/staking/StakingTab.tsx
+++ src/app/v3/pages/landing-page/tabs/staking/StakingTab.tsx
@@ -1,13 +1,10 @@
 import { FlexCol } from "@shared";
 import { StakingPage } from "./components/StakingPage";
-import { NotConnectedWalletGuard } from "../../../common/components/NotConnectedWalletGuard";
 
 export const StakingTab = () => {
   return (
     <FlexCol className="gap-8">
-      <NotConnectedWalletGuard>
-        <StakingPage />
-      </NotConnectedWalletGuard>
+      <StakingPage />
     </FlexCol>
   );
 };
--- src/app/v3/pages/landing-page/tabs/staking/components/FormContainer.tsx
+++ src/app/v3/pages/landing-page/tabs/staking/components/FormContainer.tsx
@@ -1,12 +1,11 @@
 import { useState, useEffect } from "react";
 import { FlexCol, FlexRow, Typography } from "@shared";
-// import { useParams } from "react-router-dom";
 import { Address } from "viem";
 import { FormSettingsProvider } from "../../../../../components/forms/contexts/FormSettingsContext";
 import { StakingDepositForm } from "../../../../../components/forms/safety-module-form/deposit-form/StakingDepositForm";
 import { UnstakeForm } from "../../../../../components/forms/safety-module-form/withdraw-form/UnstakeForm";
 import { InitiateCooldownForm } from "../../../../../components/forms/safety-module-form/withdraw-form/InitiateCooldownForm";
-import { useWatchStakerCooldown } from "../../../../../../statev3/safetyModule/hooks/useFetchStakerCooldown";
+import { useFetchStakerCooldown } from "../../../../../../statev3/safetyModule/hooks/useFetchStakerCooldown";
 import { useFetchCooldown } from "../../../../../../statev3/safetyModule/hooks/useFetchCooldown";
 import { useFetchUnstakeWindow } from "../../../../../../statev3/safetyModule/hooks/useFetchUnstakeWindow";
 import { STAKED_SEAM_ADDRESS } from "@meta";
@@ -18,46 +17,46 @@ const getDeadlines = (startTime: bigint, cooldown: bigint, unstakeWindow: bigint
 };
 
 export const FormContainer: React.FC = () => {
-  // const { address } = useParams();
-  const address = STAKED_SEAM_ADDRESS;
-  const vault = address as Address | undefined;
+  const vault = STAKED_SEAM_ADDRESS as Address | undefined;
   const [isDepositing, setIsDepositing] = useState(true);
   const [hasCooldown, setHasCooldown] = useState(false);
   const [remaining, setRemaining] = useState(0);
   const [isUnstakeWindow, setIsUnstakeWindow] = useState(false);
-  const { data: userCooldown } = useWatchStakerCooldown(address);
-  const { data: cooldown } = useFetchCooldown(address);
-  const { data: unstakeWindow } = useFetchUnstakeWindow(address);
+  const { data: userCooldown } = useFetchStakerCooldown(STAKED_SEAM_ADDRESS);
+  const { data: cooldown } = useFetchCooldown(STAKED_SEAM_ADDRESS);
+  const { data: unstakeWindow } = useFetchUnstakeWindow(STAKED_SEAM_ADDRESS);
 
   const userCooldownValue = userCooldown?.bigIntValue ?? 0n;
   const cooldownValue = cooldown?.bigIntValue ?? 0n;
   const unstakeWindowValue = unstakeWindow?.bigIntValue ?? 0n;
 
-  // console.log(userCooldownValue)
   useEffect(() => {
     const interval = setInterval(() => {
-      const now: number = parseInt((Date.now() / 1000).toString(), 10);
+      const now: number = Math.floor(Date.now() / 1000);
       const { canUnstakeAt, unstakeEndsAt } = getDeadlines(userCooldownValue, cooldownValue, unstakeWindowValue);
-      if (now > unstakeEndsAt || userCooldownValue === 0n) {
+      const canUnstakeAtNum = Number(canUnstakeAt);
+      const unstakeEndsAtNum = Number(unstakeEndsAt);
+
+      if (now > unstakeEndsAtNum || userCooldownValue === 0n) {
         setHasCooldown(false);
         setRemaining(0);
         clearInterval(interval);
-        return;
-      }
-
-      if (now < canUnstakeAt) {
-        const timeLeft = parseInt(canUnstakeAt.toString(), 10) - now;
-        setRemaining(timeLeft);
-        setIsUnstakeWindow(false);
       } else {
-        const timeLeft = parseInt(unstakeEndsAt.toString(), 10) - now;
-        setRemaining(timeLeft);
-        setIsUnstakeWindow(true);
+        if (now < canUnstakeAtNum) {
+          const timeLeft = canUnstakeAtNum - now;
+          setRemaining(timeLeft);
+          setIsUnstakeWindow(false);
+        } else {
+          const timeLeft = unstakeEndsAtNum - now;
+          setRemaining(timeLeft);
+          setIsUnstakeWindow(true);
+        }
+        setHasCooldown(true);
       }
-      setHasCooldown(true);
     }, 1000);
     return () => clearInterval(interval);
   }, [userCooldownValue]);
+
   return (
     <FlexCol className="bg-neutral-0 shadow-card p-6 gap-6 rounded-2xl w-full">
       <FlexRow className="items-center gap-1">
