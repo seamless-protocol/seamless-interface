241cf680bd4cc01d350fd785650a2e90286435d2
WingsDevelopment <bans.cowboy@gmail.com>
2025-04-01 22:30:58 +0200

    init

--- src/app/v3/pages/governance/components/GovInfoCard.tsx
+++ src/app/v3/pages/governance/components/GovInfoCard.tsx
@@ -2,8 +2,6 @@ import { useAccount } from "wagmi";
 import { Divider, FlexCol, FlexRow, formatAddressToDisplayable, Typography } from "@shared";
 import { GovInfoCardDataPoint } from "./GovInfoCardDataPoint";
 import { useFetchSEAMAssetBalances } from "../hooks/useFetchSeamBalances";
-import { Link } from "react-router-dom";
-import { legacyGovUrl } from "@router";
 import { useFetchDelegates } from "../../../../statev3/governance/queries/delegates/FetchDelegates.hook";
 
 export const GovInfoCard = () => {
@@ -38,17 +36,6 @@ export const GovInfoCard = () => {
 
           <GovInfoCardDataPoint label="stkSEAM" viewValue={stkSeamBalance?.tokenAmount.viewValue} {...rest} />
         </FlexRow>
-        <Divider />
-        <FlexCol className="gap-4">
-          <Typography type="medium3">
-            Use this page for delegation, this is the only place to delegate stkSEAM tokens, but use{" "}
-            <Link to={legacyGovUrl} target="_blank" className="underline">
-              legacy page
-            </Link>{" "}
-            for voting.
-          </Typography>
-          <Typography type="bold1"> Voting will soon be available here as well.</Typography>
-        </FlexCol>
       </div>
     </div>
   );
