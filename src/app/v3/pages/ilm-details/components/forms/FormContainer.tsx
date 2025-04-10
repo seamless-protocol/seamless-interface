76030a2655f5482ecb5d47f864e24ffc5380a839
WingsDevelopment <bans.cowboy@gmail.com>
2025-04-10 16:12:00 +0200

    deprecation of ilms

--- src/app/v3/pages/ilm-details/components/forms/FormContainer.tsx
+++ src/app/v3/pages/ilm-details/components/forms/FormContainer.tsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
 import { FlexCol, FlexRow, Typography } from "@shared";
 import { FormSettingsProvider } from "../../../../components/forms/contexts/FormSettingsContext";
-import { DepositForm } from "../../../../components/forms/deposit-form/DepositForm";
 import { WithdrawForm } from "../../../../components/forms/withdraw-form/WithdrawForm";
 import { useParams } from "react-router-dom";
 import { Address } from "viem";
@@ -9,41 +7,24 @@ import { Address } from "viem";
 export const FormContainer: React.FC = () => {
   const { address } = useParams();
   const strategy = address as Address | undefined;
-  const [isDepositing, setIsDepositing] = useState(true);
 
   return (
     <FlexCol className="bg-neutral-0 shadow-card p-6 gap-6 rounded-2xl w-full">
       <FlexRow className="items-center gap-1">
-        <LocalButtonSwitcher
-          data-cy="deposit-button"
-          onClick={() => {
-            setIsDepositing(true);
-          }}
-          isActive={isDepositing}
-        >
-          Deposit
-        </LocalButtonSwitcher>
+        <div className="cursor-not-allowed">
+          <LocalButtonSwitcher data-cy="deposit-button" className="cursor-not-allowed" isActive={false} disabled>
+            Deposit
+          </LocalButtonSwitcher>
+        </div>
 
-        <LocalButtonSwitcher
-          data-cy="withdraw-button"
-          onClick={() => {
-            setIsDepositing(false);
-          }}
-          isActive={!isDepositing}
-        >
+        <LocalButtonSwitcher data-cy="withdraw-button" isActive>
           Withdraw
         </LocalButtonSwitcher>
       </FlexRow>
       <div>
-        {isDepositing ? (
-          <FormSettingsProvider defaultStrategy={strategy}>
-            <DepositForm />
-          </FormSettingsProvider>
-        ) : (
-          <FormSettingsProvider defaultStrategy={strategy}>
-            <WithdrawForm />
-          </FormSettingsProvider>
-        )}
+        <FormSettingsProvider defaultStrategy={strategy}>
+          <WithdrawForm />
+        </FormSettingsProvider>
       </div>
     </FlexCol>
   );
@@ -57,8 +38,8 @@ const LocalButtonSwitcher: React.FC<LocalButtonSwitcherProps> = ({ children, onC
   return (
     <button
       onClick={onClick}
-      className={`py-4 px-8 rounded-[100px] ${isActive ? "bg-neutral-100" : "bg-white"}`}
       {...props}
+      className={`py-4 px-8 rounded-[100px] ${isActive ? "bg-neutral-100" : "bg-white"} ${props.className || ""}`}
     >
       <Typography type="bold3" className={`${isActive ? "text-primary-1000" : "text-neutral-400"}`}>
         {children}
--- src/app/v3/pages/landing-page/tabs/dashboard/table/ilms/TableButtons.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/table/ilms/TableButtons.tsx
@@ -2,38 +2,15 @@ import { useRef } from "react";
 import { Address } from "viem";
 import { ModalHandles, FlexRow, Modal } from "@shared";
 import { FormSettingsProvider } from "../../../../../../components/forms/contexts/FormSettingsContext";
-import { DepositForm } from "../../../../../../components/forms/deposit-form/DepositForm";
 import { WithdrawForm } from "../../../../../../components/forms/withdraw-form/WithdrawForm";
 
 export const TableButtons: React.FC<{
   strategy?: Address;
 }> = ({ strategy }) => {
-  const addModal = useRef<ModalHandles>(null);
   const removeModal = useRef<ModalHandles>(null);
 
   return (
     <FlexRow className="gap-2 text-start cursor-default">
-      <Modal
-        header="Deposit to Strategy"
-        ref={addModal}
-        size="normal"
-        buttonProps={{
-          "data-cy": `deposit-button-${strategy}`,
-          children: "Deposit",
-          className: "text-bold3 bg-metalic text-neutral-0 rounded-lg p-2 px-4 items-center",
-        }}
-      >
-        <div className="">
-          <FormSettingsProvider
-            defaultStrategy={strategy}
-            onTransaction={() => {
-              addModal.current?.close();
-            }}
-          >
-            <DepositForm />
-          </FormSettingsProvider>
-        </div>
-      </Modal>
       <Modal
         header="Withdraw from Strategy"
         ref={removeModal}
