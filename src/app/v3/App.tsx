5121c1d2e8be661f39372860edcbb712aa3e3cbd
WingsDevelopment <bans.cowboy@gmail.com>
2025-04-14 19:20:06 +0200

    updated path

--- src/app/v3/App.tsx
+++ src/app/v3/App.tsx
@@ -34,7 +34,7 @@ export function App() {
           </div>
 
           <FlexCol className="min-h-screen">
-            <Link to="fuul" className="text-bold3 underline text-center">
+            <Link to="/fuul" className="text-bold3 underline text-center">
               Fuul
             </Link>
             <NotificationProvider>
