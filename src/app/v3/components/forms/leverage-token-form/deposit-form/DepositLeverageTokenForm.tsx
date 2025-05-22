34767956a97ca9ebea6b83186af375a97b8b2487
WingsDevelopment <bans.cowboy@gmail.com>
2025-05-22 14:06:29 +0200

    added empty form guards, improved loading in forms.

--- src/app/v3/components/forms/leverage-token-form/deposit-form/DepositLeverageTokenForm.tsx
+++ src/app/v3/components/forms/leverage-token-form/deposit-form/DepositLeverageTokenForm.tsx
@@ -7,80 +7,54 @@ import { RHFDepositAmountField } from "./RHFDepositAmountField";
 import { RouterConfig } from "@router";
 import { RHFReceiveAmountField } from "./RHFReceiveAmountField";
 import { Summary } from "./Summary";
-import { LeverageToken } from "../../../../../data/leverage-tokens/queries/all-leverage-tokens/FetchAllLeverageTokens";
 import { useLeverageTokenFormContext } from "../leverage-token-form-provider/LeverageTokenFormProvider";
+import { EmptyFormGuard } from "../../../../pages/common/components/EmptyFormGuard";
 
 export const DepositLeverageTokenForm = () => {
-  const {
-    selectedLeverageToken: { data: leverageToken, isLoading, error },
-  } = useLeverageTokenFormContext();
-
-  if (isLoading) {
-    return <div className="min-h-[300px]" />;
-  }
-
-  if (!leverageToken || error) {
-    // eslint-disable-next-line no-console
-    console.warn("Leverage token not found!!!");
-    if (error) console.error("LeverageTokenForm error while fetching info", error);
-
-    return (
-      <div className="min-h-[300px]">
-        <Typography type="medium3" className="text-red-600">
-          Error while fetching Leverage token info: {error?.message}
-        </Typography>
-      </div>
-    );
-  }
-
-  return <LeverageFormLocal leverageToken={leverageToken} />;
-};
-
-const LeverageFormLocal: React.FC<{
-  leverageToken: LeverageToken;
-}> = ({ leverageToken }) => {
-  const { formOnSubmitAsync, methods } = useLeverageTokenFormContext();
-  const { underlyingAssetAddress } = leverageToken;
+  const { formOnSubmitAsync, methods, selectedLeverageToken } = useLeverageTokenFormContext();
+  const { underlyingAssetAddress } = selectedLeverageToken?.data || {};
 
   const onSubmitAsync = async () => {
     await formOnSubmitAsync({});
   };
 
   return (
-    <MyFormProvider methods={methods} onSubmit={methods.handleSubmit(onSubmitAsync)}>
-      <FlexCol className="gap-8">
-        <FlexCol className="gap-6">
-          <FlexCol className="gap-3">
-            <Typography type="medium3">Deposit</Typography>
-            <RHFDepositAmountField name="depositAmount" />
+    <EmptyFormGuard data={{ ...selectedLeverageToken }}>
+      <MyFormProvider methods={methods} onSubmit={methods.handleSubmit(onSubmitAsync)}>
+        <FlexCol className="gap-8">
+          <FlexCol className="gap-6">
+            <FlexCol className="gap-3">
+              <Typography type="medium3">Deposit</Typography>
+              <RHFDepositAmountField name="depositAmount" />
+            </FlexCol>
+
+            {underlyingAssetAddress === WETH_ADDRESS && (
+              <FlexRow className="w-full">
+                <Link
+                  to={RouterConfig.Routes.wrapEth}
+                  className="flex flex-row items-center justify-end gap-1"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Typography type="bold2" className="text-right">
+                    To wrap ETH, click here
+                  </Typography>
+                  <ArrowTopRightOnSquareIcon width={12} />
+                </Link>
+              </FlexRow>
+            )}
+
+            <FlexCol className="gap-3">
+              <Typography type="medium3">Receive</Typography>
+              <RHFReceiveAmountField name="receiveAmount" />
+            </FlexCol>
+
+            <Summary />
           </FlexCol>
 
-          {underlyingAssetAddress === WETH_ADDRESS && (
-            <FlexRow className="w-full">
-              <Link
-                to={RouterConfig.Routes.wrapEth}
-                className="flex flex-row items-center justify-end gap-1"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <Typography type="bold2" className="text-right">
-                  To wrap ETH, click here
-                </Typography>
-                <ArrowTopRightOnSquareIcon width={12} />
-              </Link>
-            </FlexRow>
-          )}
-
-          <FlexCol className="gap-3">
-            <Typography type="medium3">Receive</Typography>
-            <RHFReceiveAmountField name="receiveAmount" />
-          </FlexCol>
-
-          <Summary />
+          <FormButtons />
         </FlexCol>
-
-        <FormButtons />
-      </FlexCol>
-    </MyFormProvider>
+      </MyFormProvider>
+    </EmptyFormGuard>
   );
 };
--- src/app/v3/components/forms/leverage-token-form/withdraw-form/WithdrawLeverageTokenForm.tsx
+++ src/app/v3/components/forms/leverage-token-form/withdraw-form/WithdrawLeverageTokenForm.tsx
@@ -5,57 +5,34 @@ import { RHFReceiveAmountField } from "./RHFReceiveAmountField";
 import { Summary } from "./Summary";
 import { FormButtons } from "./FormButtons";
 import { useLeverageTokenFormContext } from "../leverage-token-form-provider/LeverageTokenFormProvider";
+import { EmptyFormGuard } from "../../../../pages/common/components/EmptyFormGuard";
 
 export const WithdrawLeverageTokenForm: React.FC = () => {
-  const {
-    selectedLeverageToken: { data: leverageToken, isLoading, error },
-  } = useLeverageTokenFormContext();
-
-  if (isLoading) {
-    return <div className="min-h-[300px]" />;
-  }
-
-  if (!leverageToken || error) {
-    // eslint-disable-next-line no-console
-    console.warn("Leverage Token not found!!!");
-    if (error) console.error("LeverageTokenForm error while fetching info", error);
-
-    return (
-      <div className="min-h-[300px]">
-        <Typography type="medium3" className="text-red-600">
-          Error while fetching Leverage Token info: {error?.message}
-        </Typography>
-      </div>
-    );
-  }
-
-  return <WithdrawLeverageTokenLocal />;
-};
-
-export const WithdrawLeverageTokenLocal: React.FC = () => {
-  const { methods, formOnSubmitAsync } = useLeverageTokenFormContext();
+  const { methods, formOnSubmitAsync, selectedLeverageToken } = useLeverageTokenFormContext();
 
   const onSubmit = async () => {
     await formOnSubmitAsync({});
   };
 
   return (
-    <MyFormProvider methods={methods} onSubmit={methods.handleSubmit(onSubmit)}>
-      <FlexCol className="gap-8">
-        <FlexCol className="gap-3">
-          <Typography type="medium3">Withdraw</Typography>
-          <RHFWithdrawStrategyAmountField name="withdrawAmount" />
+    <EmptyFormGuard data={{ ...selectedLeverageToken }}>
+      <MyFormProvider methods={methods} onSubmit={methods.handleSubmit(onSubmit)}>
+        <FlexCol className="gap-8">
+          <FlexCol className="gap-3">
+            <Typography type="medium3">Withdraw</Typography>
+            <RHFWithdrawStrategyAmountField name="withdrawAmount" />
+          </FlexCol>
+
+          <FlexCol className="gap-3">
+            <Typography type="medium3">Receive</Typography>
+            <RHFReceiveAmountField name="receiveAmount" />
+          </FlexCol>
+
+          <Summary />
+
+          <FormButtons />
         </FlexCol>
-
-        <FlexCol className="gap-3">
-          <Typography type="medium3">Receive</Typography>
-          <RHFReceiveAmountField name="receiveAmount" />
-        </FlexCol>
-
-        <Summary />
-
-        <FormButtons />
-      </FlexCol>
-    </MyFormProvider>
+      </MyFormProvider>
+    </EmptyFormGuard>
   );
 };
