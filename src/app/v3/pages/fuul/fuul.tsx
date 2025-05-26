62b7e9e55d3509e46fad239e2a0c0aa6e5871049
WingsDevelopment <bans.cowboy@gmail.com>
2025-05-26 09:56:57 +0200

    removed unused comment

--- src/app/v3/pages/fuul/fuul.tsx
+++ src/app/v3/pages/fuul/fuul.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useEffect } from "react";
 import { usePayoutsLeaderboard } from "../../../statev3/fuul/queries/fetch-payouts-leaderboard/PayoutsLeaderboard.hook";
 import {
