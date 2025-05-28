030f23fa4541bc505e66259e0558080b53d6a6d0
WingsDevelopment <bans.cowboy@gmail.com>
2025-05-28 14:09:28 +0200

    added feature flag

--- src/app/v3/pages/landing-page/tabs/dashboard/Dashboard.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/Dashboard.tsx
@@ -1,9 +1,10 @@
 import { FlexCol } from "@shared";
-import { PortfolioSummary } from "./portfolio-summary/Summary";
 import { TableContainer } from "./table/TableContainer";
 import { NotConnectedWalletGuard } from "../../../common/components/NotConnectedWalletGuard";
 import { LegacyPlatformDeprecationBanner } from "../../../../components/banner/LegacyPlatformDeprecationBanner";
 import { useFetchUserDepositStrategies } from "../../../../../state/loop-strategy/hooks/useFetchUserDepositStrategies";
+import { NewPortfolioSummary } from "./portfolio-summary/NewSummary";
+import { PortfolioSummary } from "./portfolio-summary/Summary";
 
 export const Dashboard = () => {
   const { data: strategies } = useFetchUserDepositStrategies();
@@ -11,7 +12,7 @@ export const Dashboard = () => {
   return (
     <FlexCol className="gap-8">
       <NotConnectedWalletGuard>
-        <PortfolioSummary />
+        {import.meta.env.VITE_LEVERAGE_TOKENS_FEATURE === "true" ? <NewPortfolioSummary /> : <PortfolioSummary />}
         {strategies && strategies.length > 0 ? <LegacyPlatformDeprecationBanner /> : null}
         <TableContainer />
       </NotConnectedWalletGuard>
--- src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/Summary.tsx
+++ src/app/v3/pages/landing-page/tabs/dashboard/portfolio-summary/Summary.tsx
@@ -1,51 +1,40 @@
 import { DisplayMoney, FlexCol, Typography } from "@shared";
 import { useFetchFormattedUserProfitAndPortfolio } from "../../../../../../statev3/hooks/user-profit-and-portfolio/UserProfitAndPortfolio.hook";
 import { Profit } from "./Profit";
-import { RewardsSelector } from "../components/claiming-rewards/RewardsSelector";
-import { RewardsProvider } from "../contexts/RewardsProvider";
+import { MorphoUnclaimedRewardsBox } from "./morpho-vaults/rewards/MorphoUnclaimedRewardsBox";
+import { useFetchViewAllUserRewards } from "../../../../../../state/lending-borrowing/hooks/useFetchViewAllRewards";
+import { UnclaimedRewardsBoxWrapper } from "./UnclaimedRewardsBoxWrapper";
 
 export const PortfolioSummary = () => {
+  const { data: rewardsData, ...restRewards } = useFetchViewAllUserRewards();
   const { data, ...rest } = useFetchFormattedUserProfitAndPortfolio();
 
   return (
-    <div className="">
-      {/* use grid instead of flex */}
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {/* Left cell: will stretch to match right */}
-        <div className="bg-blueGradient rounded-xl p-8 flex items-center justify-center text-center">
-          <FlexCol className="gap-6">
-            <div className="flex flex-col gap-3">
-              <Typography type="bold5" className="text-white">
-                Your total balance
-              </Typography>
-              <div className="w-full flex justify-center">
-                <DisplayMoney {...data.portfolioValue} {...rest} typography="bold7" className="text-white" />
-              </div>
-            </div>
-            <div className="flex flex-col gap-2 mt-10">
-              <Typography type="medium4" className="text-white">
-                Total unrealized gain/loss
-              </Typography>
-              <div className="w-full flex justify-center text-white">
+    <div>
+      <div className="bg-neutral-0 shadow-card rounded-xl">
+        <div className="flex flex-col md:flex-row gap-4 py-8  px-4 md:px-10 justify-between items-start">
+          {/* Your total balance section */}
+          <FlexCol className="gap-8 flex-grow md:basis-1/4">
+            <FlexCol className="gap-2">
+              <Typography type="bold5">Your total balance</Typography>
+              <DisplayMoney {...data.portfolioValue} {...rest} typography="bold7" />
+            </FlexCol>
+            <FlexCol className="gap-3">
+              <Typography type="medium4">Total unrealized gain/loss</Typography>
+              <div className="flex w-auto">
                 <Profit />
               </div>
-            </div>
-
-            <div className="flex flex-col gap-2">
-              <Typography type="medium4" className="text-white">
-                Total rewards claimed
-              </Typography>
-              <div className="w-full flex justify-center text-white">todo</div>
-            </div>
+            </FlexCol>
           </FlexCol>
-        </div>
 
-        {/* Right cell */}
-        <div className="flex items-center justify-center">
-          <div className="w-full">
-            <RewardsProvider>
-              <RewardsSelector />
-            </RewardsProvider>
+          {/* Reward boxes section */}
+          <div className="flex flex-col md:flex-row gap-4 flex-grow md:basis-3/4">
+            <div className="flex-grow">
+              <MorphoUnclaimedRewardsBox />
+            </div>
+            <div className="flex-grow">
+              <UnclaimedRewardsBoxWrapper {...restRewards} data={rewardsData} className="border" />
+            </div>
           </div>
         </div>
       </div>
