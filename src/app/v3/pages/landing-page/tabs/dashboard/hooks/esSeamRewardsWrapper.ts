b641b4ac87bd975f98deab4d46f5f5d9395e7cba
WingsDevelopment <bans.cowboy@gmail.com>
2025-05-29 15:56:21 +0200

    fixed vestedseam rewards

--- src/app/v3/pages/landing-page/tabs/dashboard/hooks/esSeamRewardsWrapper.ts
+++ src/app/v3/pages/landing-page/tabs/dashboard/hooks/esSeamRewardsWrapper.ts
@@ -2,25 +2,32 @@ import { SeamlessWriteAsyncParams } from "@shared";
 import { type RewardItem } from "../contexts/RewardsProvider";
 
 // todo vestedSeam icon? is icon correct on figma actually?
-import vestedSeam from "@assets/tokens/vestedSeam.svg";
-import { useFetchViewAllUserRewards } from "../../../../../../state/lending-borrowing/hooks/useFetchViewAllRewards";
+import vestedSeamIcon from "@assets/tokens/vestedSeam.svg";
 import { useMutateClaimVestedEsSEAM } from "../../../../../../statev3/governance/mutations/useMutateClaimVestedEsSEAM";
+import { useFetchVestedSeam } from "../../../../../../statev3/governance/queries/vested-seam/FetchVestedSeam.hook";
+import { ESSEAM_ADDRESS } from "../../../../../../../meta";
 
 const config = {
   id: "4",
-  icon: vestedSeam,
+  icon: vestedSeamIcon,
   name: "esSEAM rewards",
   description: "Vested esSEAM rewards",
 };
 
 export const useEsSeamRewardsWrapper = ({ settings }: { settings: SeamlessWriteAsyncParams }): RewardItem => {
   const { claimVestedAsync, isClaimVestedPending } = useMutateClaimVestedEsSEAM(settings);
-  const { data: rewardsData } = useFetchViewAllUserRewards();
+  const { data: vestedSeam } = useFetchVestedSeam();
 
   return {
     ...config,
     claimAllAsync: claimVestedAsync,
     isClaiming: isClaimVestedPending,
-    rewards: rewardsData?.rewards || [],
+    rewards: [
+      {
+        tokenAmount: vestedSeam,
+        logo: vestedSeamIcon,
+        address: ESSEAM_ADDRESS,
+      },
+    ],
   };
 };
