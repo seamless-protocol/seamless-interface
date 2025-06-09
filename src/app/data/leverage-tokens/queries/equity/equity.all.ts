03890e36a4ac5aec5bf6b93ad42ebaf343788e17
Danilo <kitanovic.danilo@outlook.com>
2025-06-09 23:00:51 +0200

    feat: update platform tvl

---
+++ src/app/data/leverage-tokens/queries/equity/equity.all.ts
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import { USD_VALUE_DECIMALS } from "../../../../../meta";
+import { formatFetchBigIntToViewBigInt } from "../../../../../shared";
+import { fetchLeverageTokens } from "../all-leverage-tokens/fetch-leverage-tokens.all";
+
+export const fetchTotalLeverageTokenEquity = async () => {
+  const leverageTokens = await fetchLeverageTokens();
+
+  let equityUsdValue: bigint | undefined;
+
+  leverageTokens.forEach(async (leverageToken) => {
+    if (leverageToken.tvl?.dollarAmount.bigIntValue) {
+      if (!equityUsdValue) {
+        equityUsdValue = leverageToken.tvl?.dollarAmount.bigIntValue;
+      } else {
+        equityUsdValue += leverageToken.tvl?.dollarAmount.bigIntValue;
+      }
+    }
+  });
+
+  return formatFetchBigIntToViewBigInt({
+    bigIntValue: equityUsdValue,
+    decimals: USD_VALUE_DECIMALS,
+    symbol: "$",
+  });
+};
+
+export const getTotalLeverageTokenEquityQueryOptions = () => ({
+  queryKey: ["useGetTotalLeverageTokenEquity"],
+  queryFn: () => fetchTotalLeverageTokenEquity(),
+});
+
+export function useFetchTotalLeverageTokenEquity() {
+  return useQuery(getTotalLeverageTokenEquityQueryOptions());
+}
--- src/app/statev3/common/hooks/useFetchPlatformTVL.tsx
+++ src/app/statev3/common/hooks/useFetchPlatformTVL.tsx
@@ -3,6 +3,7 @@ import { formatFetchBigIntToViewBigInt, formatUsdValue, mergeQueryStates } from 
 import { useFetchViewLendingPoolInfo } from "../../../v3/hooks/useFetchViewLendingPoolInfo";
 import { useFetchTotalAssetsForWhitelistedVaults } from "../../../v3/pages/morpho-vault-details/hooks/TotalAssetsHistorical.hook";
 import { USD_VALUE_DECIMALS } from "@meta";
+import { useFetchTotalLeverageTokenEquity } from "../../../data/leverage-tokens/queries/equity/equity.all";
 
 export const useFetchPlatformTVL = () => {
   const {
@@ -11,14 +12,17 @@ export const useFetchPlatformTVL = () => {
   } = useFetchViewLendingPoolInfo();
   const { data: morphoVaultsTotalAssets, ...restMorpho } = useFetchTotalAssetsForWhitelistedVaults();
 
+  const { data: leverageTokenEquity, ...restLeverageTokenEquity } = useFetchTotalLeverageTokenEquity();
+
   return {
-    ...mergeQueryStates([restLandingPool, restMorpho]),
+    ...mergeQueryStates([restLandingPool, restMorpho, restLeverageTokenEquity]),
     TVL:
-      totalMarketSizeUsd && morphoVaultsTotalAssets && totalMarketSizeUsd.value
+      totalMarketSizeUsd && morphoVaultsTotalAssets && totalMarketSizeUsd.value && leverageTokenEquity?.bigIntValue
         ? formatFetchBigIntToViewBigInt({
             ...formatUsdValue(
               parseUnits(totalMarketSizeUsd.value, USD_VALUE_DECIMALS) +
-                parseUnits(String(morphoVaultsTotalAssets), USD_VALUE_DECIMALS)
+                parseUnits(String(morphoVaultsTotalAssets), USD_VALUE_DECIMALS) +
+                leverageTokenEquity.bigIntValue
             ),
           })
         : undefined,
