20e2286a57a06943340df0f1ac8a03c46f74665c
fredwes <6827305+fredwes@users.noreply.github.com>
2025-06-30 11:20:03 -0400

    feat: remove lb nav option

--- src/app/v3/components/navigation-bar/navbarConfig.tsx
+++ src/app/v3/components/navigation-bar/navbarConfig.tsx
@@ -2,12 +2,6 @@ import { legacyGovUrl, RouterConfig } from "@router";
 
 export const navbarConfig = [
   {
-    name: "Lend / Borrow",
-    href: RouterConfig.Routes.lendingAndBorrowing,
-    isLifi: false,
-    current: false,
-  },
-  {
     name: "Swap / Bridge",
     href: "",
     isLifi: true,
